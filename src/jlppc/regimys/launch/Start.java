9cfdfd065faabc99ec6ace33bf188ec962d5cb21
Jlppc <jlppcprog@gmail.com>
2016-11-09 20:26:28 +0100

    Ajout d'indications concernant les niveaux a chaque début de combat

--- src/jlppc/regimys/launch/Start.java
+++ src/jlppc/regimys/launch/Start.java
@@ -39,7 +39,11 @@ public class Start {
 	
 	public static boolean pre = (preNbre != 0);
 	
-	public static String versionS = "" + version + (pre ? (" pre " + preNbre) : "") + commits;//Actuel : 0.04 
+	public static int sousVers = 2;
+	
+	public static boolean sousVersIs = sousVers == 0;
+	
+	public static String versionS = "" + version  + (sousVersIs ? "." + sousVers : "") + (pre ? (" pre " + preNbre) : "") + commits;//Actuel : 0.04 
 	
 	//Utilitaires pratiques
 	/**
@@ -76,7 +80,10 @@ public class Start {
 						System.out.println("Un des mots suivants doivent apparaitre après --creator :"  + Log.saut + "<html><ul><li>pokemon</li></ul></html>");
 						System.exit(2);//Les signaux d'exit du programme : 0 : Tout va bien. 1 : Erreur dans le programme 2 : Erreur dans les arguments.
 					}
-				}else{
+				}else if(args[0].equals("--version")){
+					System.out.println("Pokémon Regimys version " + versionS);
+				}
+				else{
 					System.out.println("Page d'aide des arguments de Pokemon Regimys." + Log.saut + "--creator : Permet d'affcher une fenetre de creation d'un objet (Voir --creator aide)" + Log.saut + "--help : Affiche cette page");
 				}
 				
@@ -115,7 +122,7 @@ public class Start {
 				}
 				joueur.healPoke();
 				Pokemon adv = new Pokemon(eAdv, joueur.getPoke(0).getLevel() - rand.nextInt(3), new Attaque[]{Charge.class.newInstance(), Rugissement.class.newInstance(), Soin.class.newInstance(), Triplattaque.class.newInstance()}, Caractere.HARDI, null);
-				FormattedString.outPrintln("%o vs %o", joueur.getPoke(0).getSurnom(), adv.getSurnom());
+				FormattedString.outPrintln("%o niveau %o vs %o niveau %o", joueur.getPoke(0).getSurnom(),joueur.getPoke(0).getLevel(), adv.getSurnom(), adv.getLevel());
 				Fight.fight(joueur.getPoke(0), adv);
 			}
 			
