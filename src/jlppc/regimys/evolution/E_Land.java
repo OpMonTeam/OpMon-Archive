394311dec93b5d48c9cb98c750a9e4bef6269d59
jlppc <jlppcprog@gmail.com>
2016-11-30 11:51:19 +0100

    Documentaton E_Land.java

--- src/jlppc/regimys/evolution/E_Land.java
+++ src/jlppc/regimys/evolution/E_Land.java
@@ -2,7 +2,10 @@ package jlppc.regimys.evolution;
 
 import jlppc.regimys.objects.Espece;
 import jlppc.regimys.objects.Pokemon;
-
+import jlppc.utils.WIP;
+/**Classe permettant de definir une évolution par lieu. N'est pas encore fonctionnel puisque les endroits ne sont pas encore présents
+ * @author jlppc*/
+@WIP
 public class E_Land extends Evolution {
 	
 	Object carteDuLieu;//Pour l'instant, cet objet est de type Object, car la classe permettant de determiner le lieu n'est pas crée.
