5dc8bf662ae5b1df28a62187e7656a5630b2ca77
jlppc <jlppcprog@gmail.com>
2016-11-30 11:53:48 +0100

    Documentation Evolution.java

--- src/jlppc/regimys/evolution/Evolution.java
+++ src/jlppc/regimys/evolution/Evolution.java
@@ -5,21 +5,26 @@ import java.io.Serializable;
 import jlppc.regimys.launch.Initializer;
 import jlppc.regimys.objects.Espece;
 import jlppc.regimys.objects.Pokemon;
-
+/**
+ * Classe permettant de définir une évolution, qui pourra être differente selon les cas
+ * @author jlppc
+ */
 public abstract class Evolution implements Serializable{
-	
+	/**L'espece en laquelle le pokémon evolura*/
 	protected Espece toEvolve;
+	/**Le numéro pokedex du pokemon qui évolura*/
 	protected int evo;
 	
 	public Evolution(int evo) {
 		this.evo = evo;
 	}
-	
+	/**Retourne true si l'évolution est possible, false sinon*/
 	public abstract boolean checkEvolve(Pokemon toCheck);
 	
 	public Espece getEvolution(){
 		return toEvolve;
 	}
+	/**Permet de charger l'évolution après que tous les pokémons aient été chargés*/
 	public void checkEvo(){
 		toEvolve = Initializer.listePoke[evo];
 	}
