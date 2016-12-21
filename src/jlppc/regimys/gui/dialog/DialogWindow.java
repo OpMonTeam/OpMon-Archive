ac519c087c01f3905bac7298b859cdf69e65f530
jlppc <jlppcprog@gmail.com>
2016-12-21 07:36:33 +0100

    Ajout de quelques fonctionnalitées de l'interface (NON FONCTIONNEL)

--- src/jlppc/regimys/gui/dialog/DialogWindow.java
+++ src/jlppc/regimys/gui/dialog/DialogWindow.java
@@ -1,27 +1,110 @@
 package jlppc.regimys.gui.dialog;
 
-import java.awt.EventQueue;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
-import javax.swing.border.MatteBorder;
-import java.awt.Color;
-import javax.swing.border.TitledBorder;
-import javax.swing.border.BevelBorder;
-import javax.swing.UIManager;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JPanel;
+import jlppc.regimys.core.GameState;
 
 public final class DialogWindow extends JInternalFrame {
+	JLabel text;
+	JPanel defaultPanel;
+	public static Thread keyListeners;
 	public DialogWindow() {
 		super();
-		getContentPane().setLayout(null);
-		
-		JLabel text = new JLabel("");
+		defaultPanel = new JPanel();
+		defaultPanel.setLayout(null);
+		this.setContentPane(defaultPanel);
+		text = new JLabel("");
 		text.setBounds(10, 11, 476, 98);
-		getContentPane().add(text);
+		defaultPanel.add(text);
 		
 		JLabel background = new JLabel("");
 		background.setBounds(0, 0, 496, 120);
-		getContentPane().add(background);
+		defaultPanel.add(background);
+		keyListeners = new Thread(new Runnable(){
+
+			@Override
+			public void run() {
+				defaultPanel.addKeyListener(new KeyListener(){
+
+					@Override
+					public void keyPressed(KeyEvent arg0) {
+						// TODO Auto-generated method stub
+						
+					}
+
+					@Override
+					public void keyReleased(KeyEvent arg0) {
+						if(arg0.getKeyCode() == KeyEvent.VK_Z){
+							continuer();
+						}
+						
+					}
+
+					@Override
+					public void keyTyped(KeyEvent arg0) {
+						// TODO Auto-generated method stub
+						
+					}
+					
+				});
+				
+				/*defaultPanel.addMouseListener(new MouseListener() {
+					
+					@Override
+					public void mouseReleased(MouseEvent e) {
+						continuer();
+						
+					}
+					
+					@Override
+					public void mousePressed(MouseEvent e) {
+						// TODO Auto-generated method stub
+						
+					}
+					
+					@Override
+					public void mouseExited(MouseEvent e) {
+						// TODO Auto-generated method stub
+						
+					}
+					
+					@Override
+					public void mouseEntered(MouseEvent e) {
+						// TODO Auto-generated method stub
+						
+					}
+					
+					@Override
+					public void mouseClicked(MouseEvent e) {
+						// TODO Auto-generated method stub
+						
+					}
+				});*/
+				
+			}
+			
+		});
+		keyListeners.start();
+		
+		
+	}
+	
+	public void continuer(){
+		if(GameState.state == GameState.DIALOGUE){
+			text.setText("");
+			GameState.state = GameState.MARCHE;	
+		}
+		
+	}
+	
+	public void printText(String text){
+		this.text.setText(text);
+		GameState.state = GameState.DIALOGUE;
 	}
 }
--- src/jlppc/regimys/gui/game/GameWindow.java
+++ src/jlppc/regimys/gui/game/GameWindow.java
@@ -5,18 +5,9 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public final class GameWindow extends JInternalFrame {
-	public JLabel[][][] cases = new JLabel[5][16][16];
+	
 	public GameWindow() {
 		super();
-		for(JLabel[][] c1 : cases){
-			for(JLabel[] c2 : c1){
-				for(JLabel c3 : c2){
-					c3 = new JLabel();
-					this.add(c3);
-				}
-			}
-		}
-		
 	}
 	
 	public void setPanel(JPanel panel){
