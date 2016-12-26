68e0d1d8950ac36097ffab463497fdbd516b6baf
jlppc <jlppcprog@gmail.com>
2016-12-26 11:35:50 +0100

    Revert commit 6a22455 (jlppc) "Test d'un truc sur le site"

--- menu.php
+++
@@ -1,9 +0,0 @@
-<header>
-<div class="container">
-<h1>Projet Pokemon Regimys</h1>
-
-<h2>Site du jeu Pokemon Regimys</h2>
-
-<section id="downloads"><a class="btn" href="index.html">Accueil</a> <a class="btn" href="pages/downloads.html">Téléchargements</a> <a class="btn" href="pages/doc">Documentation</a> <a class="btn" href="https://github.com/jlppc/Projet-Pokemon-Regimys/zipball/master">Télécharger le code source (.zip)</a> <a class="btn" href="https://github.com/jlppc/Projet-Pokemon-Regimys/tarball/master">Télécharger le code source (.tar.gz)</a> <a class="btn btn-github" href="https://github.com/jlppc/Projet-Pokemon-Regimys">Voir sur github</a></section>
-</div>
-</header>
