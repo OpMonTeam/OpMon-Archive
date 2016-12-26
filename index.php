df7424f68ed1ca4e90a558cac209fc669ff46b33
jlppc <jlppcprog@gmail.com>
2016-12-26 10:37:19 +0100

    Test d'un truc sur le site

--- index.php
+++ index.php
@@ -12,21 +12,7 @@
   </head>
 
    <body>
-     <header>
-      <div class="container">
-        <h1>Projet Pokemon Regimys</h1>
-        <h2>Site du jeu Pokemon Regimys</h2>
-
-        <section id="downloads">
-          <a href="index.html" class="btn">Accueil</a>
-          <a href="pages/downloads.html" class="btn">Téléchargements</a>
-          <a href="pages/doc" class="btn">Documentation</a>
-          <a href="https://github.com/jlppc/Projet-Pokemon-Regimys/zipball/master" class="btn">Télécharger le code source (.zip)</a>
-          <a href="https://github.com/jlppc/Projet-Pokemon-Regimys/tarball/master" class="btn">Télécharger le code source (.tar.gz)</a>
-          <a href="https://github.com/jlppc/Projet-Pokemon-Regimys" class="btn btn-github"><span class="icon"></span>Voir sur github</a>
-        </section>
-      </div>
-    </header>
+    <?php include("menu.php"); ?>
     <div class="container">
       <section id="main_content">
         <h1>
