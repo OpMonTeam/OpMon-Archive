223e0871519961db8fa9e09a75e8730d71ebff3a
jlppc <jlppcprog@gmail.com>
2017-01-19 13:04:46 +0100

    Travail sur la sauvegarde

--- src/jlppc/regimys/core/save/Save.java
+++ src/jlppc/regimys/core/save/Save.java
@@ -7,19 +7,24 @@ import java.io.IOException;
 import jlppc.regimys.launch.Start;
 
 public class Save {
-	static byte[] magicNumber = new byte[]{i(0xFA),i(0xCE),i(0xAF),i(0xAC),i(0xE0)};
-	static byte sep = i(0xE1);
+	static int[] magicNumber = new byte[]{0xFA,0xCE,0xAF,0xAC,0xE0};
+	static int sep = 0x88;
+	static int sepC = 0xAA;
+	static int sepV = 0xFF;
 	public static void save() throws IOException{
 		Start.playerSave.delete();
 		Start.playerSave.createNewFile();
 		BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(Start.playerSave));
-		for(int c : magicNumber){
-			bos.write(c);
+		write(magicNumber);
+		bos.write(sep);
+		int[] toSave = Start.joueur.getSave();
+		write(toSave, bos);
+		
+	}
+	
+	public static void write(int[] ints, BufferedOutputStream bos){
+		for(int inte : ints){
+			bos.write(inte);
 		}
 	}
-	
-	public static byte i(int i){
-		return (byte) i;
-	}
-	
 }
