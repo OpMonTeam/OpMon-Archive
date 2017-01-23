895f1a534d72916e25d9c7fe7bfe4a0b12b5934f
jlppc <jlppcprog@gmail.com>
2017-01-23 10:51:27 +0100

    Début du travail sur le chargement
    Aussi ajout d'une fonction pour contrer la modification de la sauvegarde

--- src/jlppc/regimys/core/save/Save.java
+++ src/jlppc/regimys/core/save/Save.java
@@ -7,10 +7,10 @@ import java.io.IOException;
 import jlppc.regimys.launch.Start;
 
 public class Save {
-	static int[] magicNumber = new byte[]{0xFA,0xCE,0xAF,0xAC,0xE0};
-	static int sep = 0x88;
-	static int sepC = 0xAA;
-	static int sepV = 0xFF;
+	static byte[] magicNumber = new byte[]{0xFA,0xCE,0xAF,0xAC,0xE0};
+	static byte sep = 0x88;
+	static byte sepC = 0xAA;
+	static byte sepV = 0xFF;
 	public static void save() throws IOException{
 		Start.playerSave.delete();
 		Start.playerSave.createNewFile();
@@ -19,6 +19,35 @@ public class Save {
 		bos.write(sep);
 		int[] toSave = Start.joueur.getSave();
 		write(toSave, bos);
+		bos.write(0x88);
+		Vector<Byte> hashCodeB = new Vector<Byte>();
+	
+		for(int ite : toSave){
+			hashCodeB.add((byte) ite);
+		}
+		String hashCode = new String(hashCodeB.toArray());
+		bos.write(hashCode.hashCode());
+		bos.close();
+		
+	}
+	
+	public static Player load(){
+		BufferedInputStream bis = new BufferedInputStream(new FileInputStream(Start.playerSave));
+		String totalSave = FileUtils.read(bis);
+		//Verification de la validité du fichier
+		String[] totalSaveA = totalSave.split(new String(new byte[]{sepC}));
+		String mn = totalSaveA[0].split(new String(new byte[]{sep}))[0];
+		if(!mn.equals(new String(magicNumber))){
+			//Lancer sauvegarde corrompue
+		}
+		int hc = (int) totalSaveA[totalSaveA.length - 1].split(new String(new byte[]{sep}))[1].charAt(0);
+		if(!(hc == totalSave.hashCode())){
+			//Lancer sauvegarde modifiée
+		}
+		//Loading
+		String[] playerSave = totalSave.split(new String(new byte[]{sep}));
+		String[] classes = playerSave[1].split(new String(new byte[]{sepC}));
+		Player.load(classes);
 		
 	}
 	
