709d804927e2c99b660e0a572f4b68c8692138f4
jlppc <jlppcprog@gmail.com>
2017-01-26 13:11:27 +0100

    Transposition des variables d'Espece en C++

--- srcCpp/jlppc/regimys/objects/Espece.hpp
+++ srcCpp/jlppc/regimys/objects/Espece.hpp
@@ -3,13 +3,19 @@
 
 #include <iostream>
 #include "srcCpp/jlppc/regimys/evolution/Evolution.hpp"
+#include "srcCpp/jlppc/regimys/enums/Type.hpp"
+#include "srcCpp/jlppc/utils/HashArray.hpp"
+#include "srcCpp/jlppc/regimys/start/main.hpp"
 
 using namespace std;
 
 class Espece
 {
     public:
-        Espece();
+        Espece(int atk, int def, int atkSpe, int defSpe, int vit, int pv, string name, Type type1, Type type2,
+			int maniereEvolution, int niveauEvolution, Evolution evolType, HashArray attacksByLevels[],
+			CT ctCombatibles[], Stats EVGiven[], float taille, float poids, string entreePokedex, int expGiven,
+int expMax, int tauxDeCapture);
 
     protected:
 
@@ -19,6 +25,25 @@ class Espece
         Espece evolution;
         int niveauEvolution;
         Evolution evolType;
+        Type type1;
+        Type type2;
+        HashArray atksByLevels[];
+        string entreePokedex;
+        float taille;
+        float poids;
+        CT ctCompatibles[];
+        int baseAtk;
+        int baseDef;
+        int baseAtkSpe;
+        int baseDefSpe;
+        int baseVit;
+        int basePV;
+        CourbeExp courbe;
+        int expMax;
+        Stats EVgiven[];
+        //Images
+        int expGiven;
+        int tauxDeCapture;
 
 };
 
