948b1685e13b2b0ea693e95ccdbd3ee7f4f5a693
jlppc <jlppcprog@gmail.com>
2017-01-31 13:12:11 +0100

    Ajout d'Item Mais pas de ses héritiers

--- srcCpp/jlppc/regimys/objects/item/Item.cpp
+++ srcCpp/jlppc/regimys/objects/item/Item.cpp
@@ -1,14 +1,61 @@
-/*
- * Item.cpp
- *
- *  Created on: 28 janv. 2017
- *      Author: Jlppc (session)
- */
 
 #include "Item.hpp"
 
-Item::Item() {
-	// TODO Auto-generated constructor stub
-
+Item::Item(string nom, bool usable, bool usableInFight, bool givable, BagCat categorie, int id) {
+    this->nom = nom;
+    this->usable = usable;
+    this->usableInFight = usableInFight;
+    this->id = id;
+    this->givable = givable;
+    this->categorie = categorie;
 }
 
+Item* Item::getItem(string const& name){
+    for(int i = 0; i < itemsList.size(); i++){
+        if(itemsList[i]->getName() == name){
+            return itemsList[i];
+        }
+    }
+}
+
+Item* Item::getItem(int id){
+    if(!(id < 0 || id > itemsList.size())){
+        return itemsList[id];
+    }else{
+        return NULL;
+    }
+}
+
+int Item::searchItem(Item const& toSearch){
+    for(int i = 0; i < itemsList.size();i++){
+        if(toSearch == *itemsList.size()){
+            return i;
+        }
+    }
+    return -1;
+}
+
+bool Item::operator==(Item const& a, Item const& b){
+    if (a.categorie != b.categorie) {
+			return false;
+		}
+		if (a.givable != b.givable) {
+			return false;
+		}
+		if (!(a.nom == b.nom)) {
+			return false;
+		}
+		if (a.usable != b.usable) {
+			return false;
+		}
+		if (a.usableInFight != b.usableInFight) {
+			return false;
+		}
+		if(a.id != b.id){
+            return false;
+		}
+        return true;
+}
+
+void Item::initItems(){
+}
--- srcCpp/jlppc/regimys/objects/item/Item.hpp
+++ srcCpp/jlppc/regimys/objects/item/Item.hpp
@@ -1,17 +1,36 @@
-/*
- * Item.hpp
- *
- *  Created on: 28 janv. 2017
- *      Author: Jlppc (session)
- */
-
 #ifndef SRCCPP_JLPPC_REGIMYS_OBJECTS_ITEM_ITEM_HPP_
 #define SRCCPP_JLPPC_REGIMYS_OBJECTS_ITEM_ITEM_HPP_
 
+#include <iostream>
+#include "../enums/Enums.hpp"
+#include <vector>
+using namespace std;
+
 class Item: public RegimysObject {
 public:
-	Item();
-	static Class<Item> *classe = new Class<Item>("Item", 4984631);
+	Item(string nom, bool usable, bool usableInFight, bool givable, BagCat categorie, int id);
+	static Class<Item>* getStatClass(){return classe;}
+	static void initItems();
+	static Item* getItem(string name);
+	static Item* getItem(int id);
+	static int searchItem(Item* const& toSearch);
+	bool operator==(Item const& a, Item const& b);
+	string getNom(){return nom;}
+	bool isUsable(){return usable;}
+	bool isGivable(){return givable;}
+	BagCat getCategorie(){return categorie;}
+	bool isUsableInFight(){return usableInFight;}
+	int getID(){return id;}
+private:
+    string nom;
+    bool usable;
+    bool givable;
+    BagCat categorie;
+    bool usableInFight;
+    int id;
+    static vector<Item*> itemsList = vector<Item>();
+
+	static const Class<Item> *classe = new Class<Item>("Item", 4984631);
 };
 
 #endif /* SRCCPP_JLPPC_REGIMYS_OBJECTS_ITEM_ITEM_HPP_ */
