3c210263a9f45322005315c380c074bb1de7a078
jlppc <jlppcprog@gmail.com>
2017-02-01 11:47:28 +0100

    Ajout de I_Pokeball en C++

---
+++ srcCpp/jlppc/regimys/objects/item/IPokeball.hpp
@@ -0,0 +1,15 @@
+
+#ifndef SRCCPP_JLPPC_REGIMYS_OBJECTS_ITEM_IPOKEBALL_HPP_
+#define SRCCPP_JLPPC_REGIMYS_OBJECTS_ITEM_IPOKEBALL_HPP_
+
+#include "Item.hpp"
+
+class I_Pokeball: public Item {
+public:
+	I_Pokeball(string nom, float tauxCapture, int ID);
+	float getTauxCapture() const{return tauxCapture;}
+private:
+	float tauxCapture;
+};
+
+#endif /* SRCCPP_JLPPC_REGIMYS_OBJECTS_ITEM_IPOKEBALL_HPP_ */
