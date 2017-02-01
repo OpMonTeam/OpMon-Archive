4e7e642ac67f36cb1f66122b2e3b2b43657cce35
jlppc <jlppcprog@gmail.com>
2017-02-01 21:37:09 +0100

    Conversion de deux premieres attaques
    en C++

--- srcCpp/jlppc/regimys/objects/Attaques.hpp
+++ srcCpp/jlppc/regimys/objects/Attaques.hpp
@@ -11,7 +11,18 @@ using namespace std;
 namespace Attaques {
 
 	class Abime : public Attaque{
+	public:
+		Abime() : Attaque("Abime", 99999, Type::SOL, 30, false, false, -1, false, 5, 0){}
+		int effetAvant(Pokemon, Pokemon);
+		int effetApres(Pokemon, Pokemon){return 0;};
+		static const Class<Abime> *classe = new Class<Abime>("Abime", 0xFFFF)
+	};
 
+	class Acidarmure : public Attaque{
+		Acidarmure() : Attaque("Acidarmure", 0, Type::POISON, 100, false, true, -1, true, 20, 0){}
+		int effetAvant(Pokemon, Pokemon){}
+		int effetApres(Pokemon, Pokemon);
+		static const Class<Abime> *classe = new Class<Acidarmure>("Acidarmure", 0xFFFE);
 	};
 
 }
