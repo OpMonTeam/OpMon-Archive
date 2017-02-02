23e9feae381a166ee6aad70efc296c87352bc5d0
jlppc <jlppcprog@gmail.com>
2017-02-02 12:48:07 +0100

    Ajout d'attaques (hpp) (Désolé celui d'avant c'est cpp)

--- srcCpp/jlppc/regimys/objects/Attaques.hpp
+++ srcCpp/jlppc/regimys/objects/Attaques.hpp
@@ -13,17 +13,39 @@ namespace Attaques {
 	class Abime : public Attaque{
 	public:
 		Abime() : Attaque("Abime", 99999, Type::SOL, 30, false, false, -1, false, 5, 0){}
-		int effetAvant(Pokemon, Pokemon);
-		int effetApres(Pokemon, Pokemon){return 0;};
+		int effetAvant(Pokemon &atk, Pokemon &def);
+		int effetApres(Pokemon &atk, Pokemon &def){return 0;};
 		static const Class<Abime> *classe = new Class<Abime>("Abime", 0xFFFF);
 	};
 
 	class Acidarmure : public Attaque{
 		Acidarmure() : Attaque("Acidarmure", 0, Type::POISON, 100, false, true, -1, true, 20, 0){}
-		int effetAvant(Pokemon, Pokemon){}
-		int effetApres(Pokemon, Pokemon);
-		static const Class<Abime> *classe = new Class<Acidarmure>("Acidarmure", 0xFFFE);
+		int effetAvant(Pokemon &atk, Pokemon &def){}
+		int effetApres(Pokemon &atk, Pokemon &def);
+		static const Class<Acidarmure> *classe = new Class<Acidarmure>("Acidarmure", 0xFFFE);
 	};
+	
+	class Acide : public Attaque{
+		Acidarmure() : Attaque("Acide", 40, Type::POISON, 100, true, false, 16, false, 30, 0){}
+		int effetAvant(Pokemon &atk, Pokemon &def){}
+		int effetApres(Pokemon &atk, Pokemon &def);
+		static const Class<Acide> *classe = new Class<Acide>("Acide", 0xFFFD);
+	}
+	
+	class Affutage : public Attaque{
+		Affutage() : Attaque("Affutage", 0, Type::NORMAL, 100, false, true, -1, true, 30, 0);
+		int effetAvant(Pokemon &atk, Pokemon &def){}
+		int effetApres(Pokemon &atk, Pokemon &def);
+		static const Class<Affutage> *classe = new Class<Affutage>("Affutage", 0xFFFC);
+	}
+	
+	class Aiguisage : public Attaque{
+		Aiguisage() : Attaque("Aiguisage", 0, Type::NORMAL, 100, false, true, -1, true, 15, 0);
+		int effetAvant(Pokemon &atk, Pokemon &def){}
+		int effetApres(Pokemon &atk, Pokemon &def);
+		static const Class<Aiguisage> *classe = new Class<Aiguisage>("Aiguisage", 0xFFFB);
+	}
+	
 
 }
 
