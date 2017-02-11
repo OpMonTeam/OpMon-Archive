af113ac3da0150cf39394afd3d95b19c8a866e5f
jlppc <jlppcprog@gmail.com>
2017-02-11 22:26:10 +0100

    Compilation avec Attaques.cpp

--- src/jlppc/utils/Class.cpp
+++ src/jlppc/utils/Class.cpp
@@ -1,17 +1,1 @@
 #include "Class.hpp"
-
-template<typename T>Class<T>::Class(string name, int id) {
-	this->name = name;
-	this->id = id;
-
-}
-
-template<typename T> T* Class<T>::newInstance() const{
-	return new T();
-}
-
-template<typename T> template<typename U> bool Class<T>::operator==(Class<U> const& y){
-	if(id == y.id){
-		return true;
-	}
-}
