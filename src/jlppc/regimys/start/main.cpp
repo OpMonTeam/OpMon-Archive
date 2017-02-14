8d4f2708f7d2d5f3939b222f203d33b40e1bef33
jlppc <jlppcprog@gmail.com>
2017-02-14 16:00:57 +0100

    Ajout du début de graphisme
    Le programme affiche mon personnage pendant 5 secondes avant de se
    fermer.

--- src/jlppc/regimys/start/main.cpp
+++ src/jlppc/regimys/start/main.cpp
@@ -13,9 +13,18 @@
 #include <SDL/SDL.h>
 #include "main.hpp"
 #include "../gui/MainFrame.hpp"
+#include <ctime>
 #define WINDOWS
 using namespace std;
 
+ #ifdef WINDOWS
+        ofstream rlog("logs\\log.txt");
+        ofstream rerrLog("logs\\errLog.txt");
+    #else
+        #define UNIX
+        ofstream rlog = ofstream("logs/log.txt");
+        ofstream rerrLog = ofstream("logs/errLog.txt")
+    #endif // WINDOWS
 namespace Main{
 
 ostringstream oss;
@@ -23,19 +32,13 @@ ostringstream oss;
     bool connected = false;
     string trainers[] = {"Brice", "Evan", "Mael", "Jlppc", "Red", "Blue", "Nikolai", "N", "Belladonis", "Aristote", "Giovanni", "Flora", "Silver", "Jules Cesar", "Gwendal", "Brahim"};
     float version = 0.07;
-    int sousVers = 0;
+    int sousVers = 1;
     string versionS;
     extern Player joueur;
     extern RFile playerSave;
     extern RFile params;
-    #ifdef WINDOWS
-    ofstream log("logs\\log.txt");
-    ofstream errLog("logs\\errLog.txt");
-    #else
-    #define UNIX
-    ofstream log = ofstream("logs/log.txt");
-    ofstream errLog = ofstream("logs/errLog.txt")
-    #endif // WINDOWS
+    long startTime = time(NULL);
+
 #ifdef WINDOWS
     string sep = "\\";
 #else
@@ -43,14 +46,16 @@ ostringstream oss;
 #endif
 
 int starts(){
-       if(!log){
+       if(!rlog){
             cout << "Erreur d'initialisation du log" << endl;
            exit(-1);
        }
-       if(!errLog){
+       if(!rerrLog){
            cout << "Erreur d'ouverture du log d'erreur" << endl;
             exit(-2);
        }
+        startTime = time(NULL);
+       rlog << "[T = " << time(NULL) - startTime << "] - Initialisation du log terminée." << endl;
 
        MainFrame::open();
 
@@ -60,11 +65,11 @@ int starts(){
 }
 
 void gererErreur(string errorName, bool fatal){
-    Main::errLog << "Erreur : " << errorName << endl;
+    rerrLog << "Erreur : " << errorName << endl;
     if(fatal){
-        Main::errLog << "Erreur fatale." << endl;
-        Main::log.close();
-        Main::errLog.close();
+        rerrLog << "Erreur fatale." << endl;
+        rlog.close();
+        rerrLog.close();
         quit(1);
     }
 }
@@ -73,8 +78,9 @@ int quit(int returne){
 	if(MainFrame::init){
 		SDL_Quit();
 	}
-	Main::log.close();
-    Main::errLog.close();
+	rlog << "[T = " << time(NULL) - Main::startTime << "] - Fermeture du jeu." << endl;
+	rlog.close();
+    rerrLog.close();
     exit(returne);
     for(unsigned int i = 0; i < POKE_NUMBER;i++){
         delete(Initializer::listePoke[i]);
@@ -85,7 +91,15 @@ int quit(int returne){
 
 int main(int argc, char *argv[])
 {
-    Main::oss << "Alpha" << Main::version << "." << Main::sousVers;
+
+    Main::oss << "Alpha " << Main::version << "." << Main::sousVers;
     Main::versionS = Main::oss.str();
+    if(argc >= 2){
+            string str = argv[1];
+        if(str == "--version"){
+            cout << "Pokémon Regimys version " << Main::versionS << endl;
+            exit(0);
+        }
+    }
     return Main::starts();
 }
