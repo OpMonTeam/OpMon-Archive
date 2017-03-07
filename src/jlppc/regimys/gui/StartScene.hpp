7c31604584fdc7c970d6768cec630f63588b57e3
jlppc <jlppcprog@gmail.com>
2017-03-07 12:24:38 +0100

    Optimisation du code (BONUS)

---
+++ src/jlppc/regimys/gui/StartScene.hpp
@@ -0,0 +1,26 @@
+#ifndef STARTSCENE_H
+#define STARTSCENE_H
+
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_ttf.h>
+#include <SDL/SDL_mixer.h>
+#include "../start/bigDefines.hxx"
+
+namespace MainFrame{
+    namespace StartScene{
+	    /**Fait se dérouler la scène de début*/
+        void startScene();
+
+        extern SDL_Rect fondP;
+		extern SDL_Rect profP;
+		extern SDL_Texture *fondT;
+		extern SDL_Texture *profT;
+		extern SDL_Texture *arrDial;
+		extern SDL_Rect arrDialP;
+		extern Mix_Music *fondMus;
+    }
+}
+
+
+#endif // STARTSCENE_H
