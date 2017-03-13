2bf8c4d8f1e9635cb29f8516d86de6b6330ad15e
jlppc <jlppcprog@gmail.com>
2017-03-13 14:40:42 +0100

    Travail sur MainMenu

--- src/jlppc/regimys/gui/MainMenu.hpp
+++ src/jlppc/regimys/gui/MainMenu.hpp
@@ -1,24 +1,29 @@
 #ifndef MAINMENU_HPP
 #define MAINMENU_HPP
 
+#include <SDL/SDL.h>
+#include "MainFrame.hpp"
+
 namespace MainFrame{
     namespace MainMenu{
 
     extern SDL_Texture *fond;
-    extern SDL_Texture *play;
-    extern SDL_Texture *charge;
-    extern SDL_Texture *options;
-    extern SDL_Texture *exit;
-    extern SDL_Texture *cursor;
-    extern SDL_Rect curPos[4];
-    extern SDL_Rect textPos[4];
+        extern SDL_Texture *play;
+        extern SDL_Texture *charge;
+        extern SDL_Texture *options;
+        extern SDL_Texture *exit;
+        extern SDL_Texture *cursor;
+        extern SDL_Rect curPos[4];
+        extern SDL_Rect textPos[4];
 
-    void initVars();
-    void verifVars();
-    void deleteVars();
-    void mainMenu();
-    void boucle0();
-}
+        extern bool continuer;
+
+        void initVars();
+        void verifVars();
+        void deleteVars();
+        void mainMenu();
+        void boucle0();
+    }
 }
 
 
