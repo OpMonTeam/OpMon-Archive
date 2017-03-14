00bfcbd43092463f1fe5ab0671061d5cff79677b
Navet56 <evandib@gmail.com>
2017-03-14 13:22:10 +0100

    Simplification du code du .sh

--- exeLinux/Install.sh
+++ exeLinux/Install.sh
@@ -1,22 +1,5 @@
-﻿echo Entrez votre mot de passe pour installer les dépendences
-sudo apt install libsdl2-2.0-0
-sudo apt install libsdl2-mixer-2.0-0
-sudo apt install libsdl2-ttf-2.0-0
-sudo apt install libsdl2-image-2.0-0
-sudo apt install libfreetype6
-sudo apt install gcc
-sudo apt install zlib1g
-sudo apt install libvorbis0a
-sudo apt install libvorbisfile3
-sudo apt install libwebp5
-sudo apt install libflac8
-sudo apt install libjpedg9
-sudo apt install libmodplug1
-sudo apt install libogg0
-sudo apt install libpng16-16
-sudo apt install libstdc++6
-sudo apt install libtiff5
-sudo apt install libsmpeg0
+﻿echo Entrez votre mot de passe pour installer les dépendences :
+sudo apt install libsdl2-2.0-0 libsdl2-mixer-2.0-0 libsdl2-ttf-2.0-0 libsdl2-image-2.0-0 libfreetype6 gcc zlib1g libvorbis0a libvorbisfile3 libwebp5 libflac8 libjpedg9 libmodplug1 libogg0 libpng16-16 libstdc++6 libtiff5 libsmpeg0
 rm ressources
 cp -r ../exe/ressources ../exeLinux
 cp -r ../exeLinux /opt
