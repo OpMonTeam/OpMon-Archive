69f7e9fd1a415b65c78e269cbb0beb9568831b3d
Navet56 <evandib@gmail.com>
2017-03-15 18:24:42 +0100

    corrections des bugs d'installation

--- exeLinux/Install.sh
+++ exeLinux/Install.sh
@@ -1,6 +1,7 @@
-echo Please enter your password to install dependences :
-sudo apt install libsdl2-2.0-0 libsdl2-mixer-2.0-0 libsdl2-ttf-2.0-0 libsdl2-image-2.0-0 libfreetype6 gcc zlib1g libvorbis0a libvorbisfile3 libwebp5 libflac8 libjpedg9 libmodplug1 libogg0 libpng16-16 libstdc++6 libtiff5 libsmpeg0
-rm ressources
+echo Please enter your password :
+su
+apt install libsdl2-2.0-0 libsdl2-mixer-2.0-0 libsdl2-ttf-2.0-0 libsdl2-image-2.0-0 libfreetype6 gcc zlib1g libvorbis0a libvorbisfile3 libwebp5 libflac8 libmodplug1 libogg0 libpng16-16 libstdc++6 libtiff5 libsmpeg0
+rm -r ressources
 cp -r ../exe/ressources ../exeLinux
 cp -r ../exeLinux /opt
 cp ../exeLinux/OPMon.desktop $PWD
