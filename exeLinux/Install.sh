e31e29d5e8b7e78617271ac70d6301f56b2f696d
Navet56 <evandib@gmail.com>
2017-03-15 18:59:56 +0100

    Update Install.sh

--- exeLinux/Install.sh
+++ exeLinux/Install.sh
@@ -3,6 +3,6 @@ su
 apt install libsdl2-2.0-0 libsdl2-mixer-2.0-0 libsdl2-ttf-2.0-0 libsdl2-image-2.0-0 libfreetype6 gcc zlib1g libvorbis0a libvorbisfile3 libwebp5 libflac8 libmodplug1 libogg0 libpng16-16 libstdc++6 libtiff5 libsmpeg0
 rm -r ressources
 cp -r ../exe/ressources ../exeLinux
-cp -r ../exeLinux /opt
-cp ../exeLinux/OPMon.desktop $PWD
+sudo cp -r ../exeLinux /opt
+sudocp ../exeLinux/OPMon.desktop $PWD
 echo Installation completed.
