8b5dbecdf11aec0cb3d00d78377421e1feb62d16
jlppc <jlppcprog@gmail.com>
2017-03-15 11:05:38 +0100

    Update Install.sh

--- exeLinux/Install.sh
+++ exeLinux/Install.sh
@@ -1,4 +1,4 @@
-﻿echo PLease enter your password :
+echo Please enter your password to install dependences :
 sudo apt install libsdl2-2.0-0 libsdl2-mixer-2.0-0 libsdl2-ttf-2.0-0 libsdl2-image-2.0-0 libfreetype6 gcc zlib1g libvorbis0a libvorbisfile3 libwebp5 libflac8 libjpedg9 libmodplug1 libogg0 libpng16-16 libstdc++6 libtiff5 libsmpeg0
 rm ressources
 cp -r ../exe/ressources ../exeLinux
