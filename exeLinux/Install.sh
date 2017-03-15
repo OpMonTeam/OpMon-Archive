0426894e8b762c22c0cf630c8cb273a66479f0ba
jlppc <jlppcprog@gmail.com>
2017-03-15 19:24:52 +0100

    Modification d'un truc.

--- exeLinux/Install.sh
+++ exeLinux/Install.sh
@@ -3,5 +3,10 @@ sudo apt install libsdl2-2.0-0 libsdl2-mixer-2.0-0 libsdl2-ttf-2.0-0 libsdl2-ima
 rm -r ressources
 cp -r ../exe/ressources ../exeLinux
 sudo cp -r ../exeLinux /opt
-sudo cp ../exeLinux/OPMon.desktop $PWD
+if [ $LANG = fr_FR.UTF-8 ]
+then
+	sudo cp ../exeLinux/OPMon.desktop $HOME/Bureau
+else
+	sudo cp ../exeLinux/OPMon.desktop $HOME/Desktop
+fi
 echo Installation completed.
