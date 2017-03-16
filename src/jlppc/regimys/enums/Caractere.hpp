ae88efcd620d28559042ea9cd4b2b255ca7da961
jlppc <jlppcprog@gmail.com>
2017-03-16 15:40:22 +0100

    Travail sur le systeme de sauvegarde (4/?) Systeme de sauvegarde complété sur l'objet OpMon

--- src/jlppc/regimys/enums/Caractere.hpp
+++ src/jlppc/regimys/enums/Caractere.hpp
@@ -21,7 +21,9 @@ class CaractereClass {
 		/**La stat que réduit le caractère*/
 		//->WaitEnum->Stats
 		int malus;
-		CaractereClass(int bonus, int malus) : bonus(bonus), malus(malus) {};
+
+		int id;
+		CaractereClass(int bonus, int malus, int id) : bonus(bonus), malus(malus), id(id) {};
 		//->DontUse
 		CaractereClass() {};
 
@@ -31,31 +33,33 @@ Enumère les caractères
 */
 //->Enum
 namespace Caractere {
-	const CaractereClass ASSURE = CaractereClass(Stats::DEF, Stats::ATK),
-						 BIZARRE = CaractereClass(Stats::RIEN, Stats::RIEN),
-						 BRAVE = CaractereClass(Stats::ATK, Stats::VIT),
-						 CALME = CaractereClass(Stats::DEFSPE, Stats::ATK),
-						 DISCRET = CaractereClass(Stats::ATKSPE, Stats::VIT),
-						 DOCILE = CaractereClass(Stats::RIEN, Stats::RIEN),
-						 DOUX = CaractereClass(Stats::ATKSPE, Stats::DEF),
-						 FOUFOU = CaractereClass(Stats::ATKSPE, Stats::DEFSPE),
-						 GENTIL = CaractereClass(Stats::DEFSPE, Stats::DEF),
-						 HARDI = CaractereClass(Stats::RIEN, Stats::RIEN),
-						 JOVIAL = CaractereClass(Stats::VIT, Stats::ATKSPE),
-						 LACHE = CaractereClass(Stats::DEF, Stats::DEFSPE),
-						 MALIN = CaractereClass(Stats::DEF, Stats::ATKSPE),
-						 MALPOLI = CaractereClass(Stats::DEFSPE, Stats::VIT),
-						 MAUVAIS = CaractereClass(Stats::ATK, Stats::DEFSPE),
-						 MODESTE = CaractereClass(Stats::ATKSPE, Stats::ATK),
-						 NAIF = CaractereClass(Stats::VIT, Stats::DEFSPE),
-						 PRESSE = CaractereClass(Stats::VIT, Stats::DEF),
-						 PRUDENT = CaractereClass(Stats::DEFSPE, Stats::ATKSPE),
-						 PUDIQUE = CaractereClass(Stats::RIEN, Stats::RIEN),
-						 RELAX = CaractereClass(Stats::DEF, Stats::VIT),
-						 RIGIDE = CaractereClass(Stats::ATK, Stats::ATKSPE),
-						 SERIEUX = CaractereClass(Stats::RIEN, Stats::RIEN),
-						 SOLO = CaractereClass(Stats::ATK, Stats::DEF),
-						 TIMIDE = CaractereClass(Stats::VIT, Stats::ATK);
+	const CaractereClass ASSURE = CaractereClass(Stats::DEF, Stats::ATK, 0),
+						 BIZARRE = CaractereClass(Stats::RIEN, Stats::RIEN, 1),
+						 BRAVE = CaractereClass(Stats::ATK, Stats::VIT, 2),
+						 CALME = CaractereClass(Stats::DEFSPE, Stats::ATK, 3),
+						 DISCRET = CaractereClass(Stats::ATKSPE, Stats::VIT, 4),
+						 DOCILE = CaractereClass(Stats::RIEN, Stats::RIEN, 5),
+						 DOUX = CaractereClass(Stats::ATKSPE, Stats::DEF, 6),
+						 FOUFOU = CaractereClass(Stats::ATKSPE, Stats::DEFSPE, 7),
+						 GENTIL = CaractereClass(Stats::DEFSPE, Stats::DEF, 8),
+						 HARDI = CaractereClass(Stats::RIEN, Stats::RIEN, 9),
+						 JOVIAL = CaractereClass(Stats::VIT, Stats::ATKSPE, 10),
+						 LACHE = CaractereClass(Stats::DEF, Stats::DEFSPE, 11),
+						 MALIN = CaractereClass(Stats::DEF, Stats::ATKSPE, 12),
+						 MALPOLI = CaractereClass(Stats::DEFSPE, Stats::VIT, 13),
+						 MAUVAIS = CaractereClass(Stats::ATK, Stats::DEFSPE, 14),
+						 MODESTE = CaractereClass(Stats::ATKSPE, Stats::ATK, 15),
+						 NAIF = CaractereClass(Stats::VIT, Stats::DEFSPE, 16),
+						 PRESSE = CaractereClass(Stats::VIT, Stats::DEF, 17),
+						 PRUDENT = CaractereClass(Stats::DEFSPE, Stats::ATKSPE, 18),
+						 PUDIQUE = CaractereClass(Stats::RIEN, Stats::RIEN, 19),
+						 RELAX = CaractereClass(Stats::DEF, Stats::VIT, 20),
+						 RIGIDE = CaractereClass(Stats::ATK, Stats::ATKSPE, 21),
+						 SERIEUX = CaractereClass(Stats::RIEN, Stats::RIEN, 22),
+						 SOLO = CaractereClass(Stats::ATK, Stats::DEF, 23),
+						 TIMIDE = CaractereClass(Stats::VIT, Stats::ATK, 24);
+    const CaractereClass *enumsList[25] = {&ASSURE, &BIZARRE, &BRAVE, &CALME, &DISCRET, &DOCILE, &DOUX, &FOUFOU, &GENTIL, &HARDI, &JOVIAL, &LACHE, &MALIN, &MALPOLI, &MAUVAIS, &MODESTE,
+                                        &NAIF, &PRESSE, &PRUDENT, &PUDIQUE, &RELAX, &RIGIDE, &SERIEUX/*Why so serious?*/,&SOLO, &TIMIDE};
 };
 
 #endif /* SRCCPP_JLPPC_REGIMYS_ENUMS_CARACTERE_HPP_ */
