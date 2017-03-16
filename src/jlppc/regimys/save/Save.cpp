a5df473498ee0274115c298890aa42e603cee12d
jlppc <jlppcprog@gmail.com>
2017-03-16 13:31:57 +0100

    Travail sur la sauvegarde

--- src/jlppc/regimys/save/Save.cpp
+++ src/jlppc/regimys/save/Save.cpp
@@ -8,9 +8,12 @@ UNS
 namespace Save{
     ostringstream saveOutput;
     int save(Player* toSave, string fileOut){
-        ofstream outStream(fileOut);
+        ofstream outStream(fileOut.c_str());
         string outStr;
         saveOutput << "OP_SAVE" << endl;
-        toSave.save();
+        toSave->save();
     }
+
+    int charToInt(char toInt) {return toInt;}
+    char intToChar(int toChar) {return toChar;}
 }
