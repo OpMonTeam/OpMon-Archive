e02e577477fa6e9ebe7c1c53fcd039e1e4f3571d
jlppc <jlppcprog@gmail.com>
2017-03-18 19:04:09 +0100

    Travail sur le systeme de sauvegarde (6/?)

--- src/jlppc/utils/Utils.cpp
+++ src/jlppc/utils/Utils.cpp
@@ -1,4 +1,5 @@
 #include "Utils.hpp"
+#include <iostream>
 
 int Utils::randU(int limit) {
 	srand(time(NULL));
@@ -13,4 +14,15 @@ unsigned int Utils::randUI(unsigned int limit) {
 
 void Utils::wait(int ticks){
     for(int i = 0; i < ticks; i++){}
+}
+
+long Utils::hash(std::string str){
+    int code = 42;//Life and universe!
+    int othercode = 56;//Get the reference
+    int thirdcode = str[0];
+    long hash = 12;
+    for(unsigned int i = 0; i < str.size();i++){
+        hash += (str[i] * code + (hash/othercode) / str[0]);
+    }
+    return hash;
 }
