e3b29340839751d58211c54bbe4e7c7795c33f49
jlppc <jlppcprog@gmail.com>
2017-03-22 19:46:28 +0100

    Suppression de trucs inutiles Changement d'idée pour réaliser l'overworld

--- src/jlppc/regimys/gui/Elements.cpp
+++ src/jlppc/regimys/gui/Elements.cpp
@@ -1,23 +1,19 @@
 #include "Elements.hpp"
 
-Case::Case(SDL_Texture *texture){
-    this->texture = texture;
-}
-
-Plan::Plan(Couche couche1, Couche couche2, EventMap events, Couche couche3){
+Plan::Plan(SDL_Texture *couche1, SDL_Texture *couche2, SDL_Texture *couche3){
     this->couche1 = couche1;
     this->couche2 = couche2;
-    this->events = events;
+    //this->events = events;
     this->couche3 = couche3;
 }
 
-Plan::~Plan(){
+/*Plan::~Plan(){
     for(unsigned int i = 0; i < 32; i++){
         for(unsigned int j = 0; j < 32; j++){
             delete(events[i][j]);
         }
     }
-}
+}*/
 
 namespace Elements {
 
