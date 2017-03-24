52741dff65ea18e097f48b8cab0b1dad43f65d91
jlppc <jlppcprog@gmail.com>
2017-03-24 13:37:52 +0100

    Début de l'ajout des collisions de Fauxbourg euvi

--- src/jlppc/regimys/gui/Elements.cpp
+++ src/jlppc/regimys/gui/Elements.cpp
@@ -1,10 +1,21 @@
 #include "Elements.hpp"
+#include <cstdlib>
 
-Plan::Plan(SDL_Texture *couche1, SDL_Texture *couche2, SDL_Texture *couche3){
+Plan::Plan(SDL_Texture *couche1, SDL_Texture *couche2, SDL_Texture *couche3, int w, int h, vector<bool> passTab[]){
     this->couche1 = couche1;
     this->couche2 = couche2;
     //this->events = events;
     this->couche3 = couche3;
+    this->w = w;
+    this->h = h;
+    this->passTab = (vector<bool>*)malloc(h * w * sizeof(vector<bool>));
+    for(unsigned int i = 0; i < (w * h); i++){
+        this->passTab[i] = passTab[i];
+    }
+}
+
+Plan::~Plan(){
+    free(passTab);
 }
 
 /*Plan::~Plan(){
