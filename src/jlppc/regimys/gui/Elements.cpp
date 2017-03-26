bb7381dae926e9c7f541a0a653352c8912f013a4
jlppc <jlppcprog@gmail.com>
2017-03-26 16:18:33 +0200

    Ajout des boites de collision

--- src/jlppc/regimys/gui/Elements.cpp
+++ src/jlppc/regimys/gui/Elements.cpp
@@ -2,27 +2,33 @@
 #include <cstdlib>
 #include "../start/main.hpp"
 #include "../start/Initializer.hpp"
+#include <cstdlib>
 UNS
-Plan::Plan(SDL_Texture *couche1, SDL_Texture *couche2, SDL_Texture *couche3, int w, int h, int tabNbre){
+//Idée : Enregistrer les collisions dans un fichier temporaire qui sera supprimé juste après
+Plan::Plan(SDL_Texture *couche1, SDL_Texture *couche2, SDL_Texture *couche3, int w, int h, string filename){
     this->couche1 = couche1;
     this->couche2 = couche2;
     //this->events = events;
     this->couche3 = couche3;
     this->w = w;
     this->h = h;
-    rerrLog << "PlanCheck1" << endl;
     this->passTab = (int**)malloc(h * sizeof(int*));
     for(unsigned int i = 0; i < h; i++){
         this->passTab[i] = (int*)malloc(w * sizeof(int));
     }
-    rerrLog << "PlanCheck2" << endl;
-    for(unsigned int i = 0; i < h; i++){
-        rerrLog << "i = " << i << endl;
-        for(unsigned int j = 0; j < w; j++){
-            rerrLog << "j = " << j << "passTab = " << (Initializer::tabs[tabNbre][i][j]) << endl;
-            this->passTab[i][j] = (Initializer::tabs[tabNbre][i][j]);
+    ifstream inTemp(filename.c_str());
+    for(int i = 0; i < h; i++){
+        for(int j = 0; j < w; j++){
+            inTemp >> passTab[i][j];
+            rerrLog << "Plan i=" << i << " et j=" << j << " est égal a " << passTab[i][j] << endl;
         }
     }
+    #ifdef _WIN32
+    system((string("DEL ") + filename).c_str());
+    #else
+    system((string("rm ") + filename).c_str());
+    #endif // _WIN32
+
 }
 
 Plan::~Plan(){
