ca624633a2e66cc14795f8ce29c624f68b90a173
jlppc <jlppcprog@gmail.com>
2017-03-27 17:40:49 +0200

    Accélération de la vitesse du personnage.

--- src/jlppc/regimys/gui/Overworld.cpp
+++ src/jlppc/regimys/gui/Overworld.cpp
@@ -13,6 +13,7 @@ namespace MainFrame {
         Plan *actuel = NULL;
         SDL_Texture *spritePP[4] = {};
         SDL_Texture *marchePP[4] = {};
+        SDL_Texture *marche2PP[4] = {};
         SDL_Rect ppPos = {};
         SDL_Rect mapPos = {};
         int moving = -1;//Signale le mouvement a la boucle
@@ -28,13 +29,8 @@ namespace MainFrame {
             if(anim == -1){
                 startFrames = frames;
                 anim = DOS;
-                //moving = DOS;
-                //return;
-                rerrLog << actuel->getH()*SQUARE << endl;
-                rerrLog << (ppPos.y/SQUARE + mapPos.y/SQUARE + 1) << endl;
-                rerrLog << ppPos.y/SQUARE << endl;
-                rerrLog << mapPos.y/SQUARE << endl;
-                rerrLog << ppPos.y/SQUARE + mapPos.y/SQUARE << endl;
+                moving = DOS;
+                return;
                 if((ppPos.y/SQUARE + mapPos.y/SQUARE + 1) < (actuel->getH()*SQUARE)){
                     if(actuel->getPassTab()[(ppPos.y / SQUARE) + 1][ppPos.x / SQUARE] == 0){
 
@@ -52,8 +48,8 @@ namespace MainFrame {
             if(anim == -1){
                 startFrames = frames;
                 anim = FACE;
-                //moving = FACE;
-                //return;
+                moving = FACE;
+                return;
                 if((ppPos.y/SQUARE + mapPos.y/SQUARE - 1) > 0){
                     if(actuel->getPassTab()[(ppPos.y / SQUARE) - ((ppPos.y/SQUARE <= 0) ? 0 : 1)][ppPos.x / SQUARE] == 0){
                         //Ensuite faudra faire la verif du passages des events
@@ -67,8 +63,8 @@ namespace MainFrame {
             if(anim == -1){
                 startFrames = frames;
                 anim = DROITE;
-                //moving = DROITE;
-                //return;
+                moving = DROITE;
+                return;
                 if((ppPos.x/SQUARE + mapPos.x/SQUARE + 1) < (actuel->getW()*SQUARE)){
                     if(actuel->getPassTab()[(ppPos.y / SQUARE)][(ppPos.x / SQUARE) + 1] == 0){
                         //Ensuite faudra faire la verif du passages des events
@@ -84,8 +80,8 @@ namespace MainFrame {
             if(anim == -1){
                 startFrames = frames;
                 anim = GAUCHE;
-                //moving = GAUCHE;
-                //return;
+                moving = GAUCHE;
+                return;
                 if((ppPos.x/SQUARE + mapPos.x/SQUARE - 1) > 0){
                     if(actuel->getPassTab()[(ppPos.y / SQUARE)][(ppPos.x / SQUARE) - ((ppPos.x/SQUARE <= 0) ? 0 : 1)] == 0){
                         //Ensuite faudra faire la verif du passages des events
@@ -107,6 +103,10 @@ namespace MainFrame {
             marchePP[DROITE] = IMG_LoadTexture(renderer, "ressources\\sprites\\chara\\pp\\mpp1.png");
             marchePP[GAUCHE] = IMG_LoadTexture(renderer, "ressources\\sprites\\chara\\pp\\mpp2.png");
             marchePP[DOS] = IMG_LoadTexture(renderer, "ressources\\sprites\\chara\\pp\\mpp3.png");
+            marche2PP[FACE] = IMG_LoadTexture(renderer, "ressources\\sprites\\chara\\pp\\mpp20.png");
+            marche2PP[DROITE] = IMG_LoadTexture(renderer, "ressources\\sprites\\chara\\pp\\mpp21.png");
+            marche2PP[GAUCHE] = IMG_LoadTexture(renderer, "ressources\\sprites\\chara\\pp\\mpp22.png");
+            marche2PP[DOS] = IMG_LoadTexture(renderer, "ressources\\sprites\\chara\\pp\\mpp23.png");
             #else
             spritePP[FACE] = IMG_LoadTexture(renderer, "ressources/sprites/chara/pp/pp0.png");
             spritePP[DROITE] = IMG_LoadTexture(renderer, "ressources/sprites/chara/pp/pp1.png");
@@ -116,13 +116,17 @@ namespace MainFrame {
             marchePP[DROITE] = IMG_LoadTexture(renderer, "ressources/sprites/chara/pp/mpp1.png");
             marchePP[GAUCHE] = IMG_LoadTexture(renderer, "ressources/sprites/chara/pp/mpp2.png");
             marchePP[DOS] = IMG_LoadTexture(renderer, "ressources/sprites/chara/pp/mpp3.png");
+            marche2PP[FACE] = IMG_LoadTexture(renderer, "ressources/sprites/chara/pp/mpp20.png");
+            marche2PP[DROITE] = IMG_LoadTexture(renderer, "ressources/sprites/chara/pp/mpp21.png");
+            marche2PP[GAUCHE] = IMG_LoadTexture(renderer, "ressources/sprites/chara/pp/mpp22.png");
+            marche2PP[DOS] = IMG_LoadTexture(renderer, "ressources/sprites/chara/pp/mpp23.png");
             #endif // _WIN32
             mapPos.x = -(8*SQUARE);
             mapPos.y = -(8*SQUARE);
             mapPos.h = 1024;
             mapPos.w = 1024;
             ppPos.x = mapPos.x + (16*SQUARE) + 16;
-            ppPos.y = mapPos.y + (14*SQUARE) + 16;
+            ppPos.y = mapPos.y + (14*SQUARE);
             ppPos.w = 64;
             ppPos.h = 64;
 
@@ -154,10 +158,11 @@ namespace MainFrame {
         }
 
         int boucle() {
-
+            bool anims = false;
+            int animsCounter = 0;
             while(continuer) {
                 while (continuer) {
-                    if ((SDL_GetTicks() - ancientTick) >= FPS_TICKS) {
+                    if ((SDL_GetTicks() - ancientTick) >= (FPS_TICKS)) {
                         frames++;
 
                         ancientTick = SDL_GetTicks();
@@ -204,58 +209,11 @@ namespace MainFrame {
                                 }
                                 break;*/
                         }
-
-                        if(anim == DOS){
-                            if(frames - startFrames == 16){
-                                anim = -1;
-                            }else{
-                                if(moving == DOS){
-                                    //ppPos.y = ppPos.y + 1;
-                                    mapPos.y = mapPos.y - 2;
-                                }
-                            }
-                            ppDir = FACE;
-                        }else if(anim == FACE){
-                            if(frames - startFrames == 16){
-                                anim = -1;
-                            }else{
-                                if(moving == FACE){
-                                    //ppPos.y = ppPos.y - 1;
-                                    mapPos.y = mapPos.y + 2;
-                                }
-
-                            }
-
-                            ppDir = DOS;
-                        }else if(anim == DROITE){
-                            if(frames - startFrames == 16){
-                                anim = -1;
-                            }else{
-                                if(moving == DROITE){
-                                    //ppPos.x = ppPos.x + 1;
-                                    mapPos.x = mapPos.x - 2;
-                                }
-
-                            }
-
-                            ppDir = DROITE;
-                        }else if(anim == GAUCHE){
-                            if(frames - startFrames == 16){
-                                anim = -1;
-                            }else{
-                                if(moving == GAUCHE){
-                                    //ppPos.x = ppPos.x - 1;
-                                    mapPos.x = mapPos.x + 2;
-                                }
-
-                            }
-                            ppDir = GAUCHE;
-                        }
                         SDL_RenderClear(renderer);
                         SDL_RenderCopy(renderer, actuel->getCouche1(), NULL, &mapPos);
                         SDL_RenderCopy(renderer, actuel->getCouche2(), NULL, &mapPos);
 
-                        if(anim != -1 && frames - startFrames <= 8){
+                        if(anim != -1 && !anims){
                             if(anim == DOS){
                                 SDL_RenderCopy(renderer, marchePP[FACE], NULL, &ppPos);
                             }else if(anim == FACE){
@@ -263,17 +221,84 @@ namespace MainFrame {
                             }else{
                                 SDL_RenderCopy(renderer, marchePP[anim], NULL, &ppPos);
                             }
+                            animsCounter++;
+                            anims = animsCounter > 8;
 
-                        }else{
+                        }else if(anim != -1 && anims){
+                            if(anim == DOS){
+                                SDL_RenderCopy(renderer, marche2PP[FACE], NULL, &ppPos);
+                            }else if(anim == FACE){
+                                SDL_RenderCopy(renderer, marche2PP[DOS], NULL, &ppPos);
+                            }else{
+                                SDL_RenderCopy(renderer, marche2PP[anim], NULL, &ppPos);
+                            }
+                            animsCounter++;
+                            if(animsCounter > 16){
+                                anims = false;
+                                animsCounter = 0;
+                            }
+                        }
+                        else{
                             SDL_RenderCopy(renderer, spritePP[ppDir], NULL, &ppPos);
                         }
 
                         SDL_RenderCopy(renderer, actuel->getCouche3(), NULL, &mapPos);
+
+                        if(anim == DOS){
+                            if(frames - startFrames == 8){
+                                anim = -1;
+                                //mapPos.y = mapPos.y - 4;
+                            }else{
+                                if(moving == DOS){
+                                    //ppPos.y = ppPos.y + 1;
+                                    mapPos.y = mapPos.y - 4;
+                                }
+                            }
+                            ppDir = FACE;
+                        }else if(anim == FACE){
+                            if(frames - startFrames == 8){
+                                anim = -1;
+                                //mapPos.y = mapPos.y + 4;
+                            }else{
+                                if(moving == FACE){
+                                    //ppPos.y = ppPos.y - 1;
+                                    mapPos.y = mapPos.y + 4;
+                                }
+
+                            }
+
+                            ppDir = DOS;
+                        }else if(anim == DROITE){
+                            if(frames - startFrames == 8){
+                                anim = -1;
+                               // mapPos.x = mapPos.x - 4;
+                            }else{
+                                if(moving == DROITE){
+                                    //ppPos.x = ppPos.x + 1;
+                                    mapPos.x = mapPos.x - 4;
+                                }
+
+                            }
+
+                            ppDir = DROITE;
+                        }else if(anim == GAUCHE){
+                            if(frames - startFrames == 8){
+                                anim = -1;
+                                //mapPos.x = mapPos.x + 4;
+                            }else{
+                                if(moving == GAUCHE){
+                                    //ppPos.x = ppPos.x - 1;
+                                    mapPos.x = mapPos.x + 4;
+                                }
+
+                            }
+                            ppDir = GAUCHE;
+                        }
+
                         SDL_RenderPresent(renderer);
 
-
                     } else {
-                        SDL_Delay(FPS_TICKS - (SDL_GetTicks() - ancientTick));
+                        SDL_Delay((FPS_TICKS) - (SDL_GetTicks() - ancientTick));
                     }
                 }
             }
