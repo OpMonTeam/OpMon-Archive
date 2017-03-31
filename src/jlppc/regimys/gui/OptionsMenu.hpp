15408ee2457b98d0b221f44e18f1630bac4c38bf
jlppc <jlppcprog@gmail.com>
2017-03-31 14:36:19 +0200

    Ajout des fichiers du menu des options

---
+++ src/jlppc/regimys/gui/OptionsMenu.hpp
@@ -0,0 +1,19 @@
+#ifndef OPTIONSMENU_HPP
+#define OPTIONSMENU_HPP
+
+#include <iostream>
+#include "MainFrame.hpp"
+
+namespace MainFrame{
+namespace OptionsMenu{
+
+int OptionsMenu();
+void initVars();
+void verifVars();
+void deleteVars();
+int boucle();
+
+}
+}
+
+#endif // OPTIONSMENU_HPP
