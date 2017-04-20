9b9a705f269102354a6d19099cbff77d2ec6ee07
jlppc <jlppcprog@gmail.com>
2017-04-20 21:03:11 +0200

    Début du travail sur le systeme de save des options

---
+++ src/jlppc/regimys/save/OptionsSave.hpp
@@ -0,0 +1,46 @@
+#ifndef OPTIONSSAVE_HPP
+#define OPTIONSSAVE_HPP
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include "../start/StringKeys.hpp"
+
+//Ces deux lignes sont la pour faire apparaitre le contexte de l'utilisation de la methode dans le code.
+#define addParam addOrModifParam
+#define modifyParam addOrModifParam
+
+
+namespace OptionsSave{
+
+    class Param{
+        private:
+        std::string paramName;
+        std::string valeur;
+        public:
+        Param(std::string name, std::string valeur);
+        std::string getName() const;
+        std::string getValue() const;
+        void setValue(std::string value);
+
+    };
+
+    std::vector<Param> paramList;
+
+    Param getParam(std::string const& nom);
+
+    void addOrModifParam(std::string const& nom, std::string const& value);
+
+    Param deleteParam(std::string const& nom);
+
+    //CPP ONLY :
+    //int searchParam(string nom);
+
+    void saveParams();
+
+    void initParams();
+
+}
+
+#endif // OPTIONSSAVE_HPP
