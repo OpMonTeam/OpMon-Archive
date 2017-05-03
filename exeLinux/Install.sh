33d57c734f8336bc7036b323c22d4c14eb4fef74
jlppc <jlppcprog@gmail.com>
2017-05-03 15:04:23 +0200

    Travail sur le systeme de mise a jour

--- exeLinux/Install.sh
+++ exeLinux/Install.sh
@@ -1,5 +1,5 @@
 echo Please enter your password :
-sudo apt install libsdl2-2.0-0 libsdl2-mixer-2.0-0 libsdl2-ttf-2.0-0 libsdl2-image-2.0-0 libfreetype6 gcc zlib1g libvorbis0a libvorbisfile3 libwebp5 libflac8 libmodplug1 libogg0 libpng16-16 libstdc++6 libtiff5 libsmpeg0
+sudo apt install libsdl2-2.0-0 libsdl2-mixer-2.0-0 libsdl2-ttf-2.0-0 libsdl2-image-2.0-0 libfreetype6 gcc zlib1g libvorbis0a libvorbisfile3 libwebp5 libflac8 libmodplug1 libogg0 libpng16-16 libstdc++6 libtiff5 libsmpeg0 curl
 rm -r ressources
 cp -r ../exe/ressources ../exeLinux
 sudo cp -r ../exeLinux /opt
