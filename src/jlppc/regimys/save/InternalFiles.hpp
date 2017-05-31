f01ffd859c1303400c51a7c1cfda17bb3789c7e1
jlppc <jlppcprog@gmail.com>
2017-05-31 09:57:26 +0200

    Début d'ajout d'un système de fichier virtuel interne au programme

---
+++ src/jlppc/regimys/save/InternalFiles.hpp
@@ -0,0 +1,17 @@
+#ifndef INTERNALFILES_HPP
+#define INTERNALFILES_HPP
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+namespace InternalFiles
+{
+    extern std::vector<std::string> filesNames;
+    extern std::vector<std::string> files;
+    void registerFiles();
+    std::ostringstream* openFileOut(std::string name);
+    std::istringstream* openFileIn(std::string name);
+};
+
+#endif // INTERNALFILES_HPP
