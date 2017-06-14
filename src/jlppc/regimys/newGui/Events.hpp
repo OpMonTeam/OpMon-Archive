96ea18e2966cb0904c77de5b5d096946a9a7e25c
jlppc <jlppcprog@gmail.com>
2017-06-14 08:46:16 +0200

    Ajout du la classe de base Event Début du travail sur les events

--- src/jlppc/regimys/newGui/Events.hpp
+++ src/jlppc/regimys/newGui/Events.hpp
@@ -9,12 +9,34 @@ Contient le namespace Events
 #define EVENTS_HPP
 #include "Elements.hpp"
 #include "MainFrame.hpp"
+#include "../enums/Enums.hpp"
+/**
+Contient la base de tous les types d'events
+*/
+class Event{
+    protected:
+    sf::Texture baseTexture;
+    std::vector<sf::Texture> otherTextures;
+    sf::Sprite *sprite;
+    //WaitEnum->EventTrigger
+    int eventTrigger = 0;
+    sf::Vector2i position;
+    public:
+    Event(sf::Texture baseTexture, std::vector<sf::Texture> otherTextures, int eventTrigger, sf::Vector2i position);
+    virtual ~Event();
+    /**Methode appelée chaque frame*/
+    virtual update() = 0;
+    /**Methode appellée lors du contact avec l'event (voir eventTrigger)*/
+    virtual action() = 0;
+}
 
 /**
 Contient tout ce qui est en rapport avec les evenements
 */
 namespace Events {
-
+    namespace EventTrigger{
+        const int PRESS = 0, TOUCH = 1, ZONE = 2, IN = 3;
+    }
 }
 
 #endif // EVENTS_HPP
