d7fc57cf9dc1df4a0f621b4500131e8ba54e9782
jlppc <jlppcprog@gmail.com>
2017-06-16 13:42:55 +0200

    Ajout des definissions de quelques constructeurs d'Event

--- src/jlppc/regimys/newGui/Events.cpp
+++ src/jlppc/regimys/newGui/Events.cpp
@@ -1,2 +1,26 @@
 #include "Events.hpp"
 
+UNS
+
+Event::Event(sf::Texture baseTexture, std::vector<sf::Texture> otherTextures, int eventTrigger, sf::Vector2i position, bool passable) :
+baseTexture(baseTexture), otherTextures(otherTextures), eventTrigger(eventTrigger), position(position), passable(passable){
+    sprite = new sf::Sprite();
+    sprite.setTexture(this->baseTexture);
+    sprite.setPosition(this->position);
+}
+
+namespace Events{
+
+TPEvent::TPEvent(sf::Texture baseTexture, std::vector<sf::Texture> otherTextures, int eventTrigger, sf::Vector2i position, sf::Vector2i tpPos, int mapID):
+Event(baseTexture, otherTextures, eventTrigger, position, passable), tpCoord(tpPos), mapID(mapID){
+
+}
+
+DoorEvent::DoorEvent(int doorType, sf::Vector2i position, sf::Vector2i tpPos, int eventTrigger = 3):
+TPEvent(selectDoorType(doorType), selectDoorTypeOther(doorType), eventTrigger, position, tpPos, mapID), doorType(doorType){
+
+}
+
+
+
+}
