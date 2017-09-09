c97ad73cc73207e46469184ca21695a1bc4bb0d8
jlppc <jlppcprog@ntymail.com>
2017-09-09 12:27:41 +0200

    Add laboratory unglitched

--- src/jlppc/regimys/newGui/Elements.cpp
+++ src/jlppc/regimys/newGui/Elements.cpp
@@ -20,7 +20,7 @@ MapLayer::MapLayer(sf::Vector2i size, const int tilesCodes[]){
       int tileNumber = tilesCodes[(i * size.x) + j] - 1;
 
       if(tileNumber == -1){
-	tileNumber = 258;//Void tile
+	tileNumber = 257;//Void tile
       }
       
       int tx = tileNumber % (tileset.getSize().x / 32);
@@ -56,9 +56,6 @@ Map::Map(const int layer1[], const int layer2[], const int layer3[], int w, int 
   this->layer1 = new MapLayer(sf::Vector2i(w + 16, h + 16), layer1);
     this->layer2 = new MapLayer(sf::Vector2i(w + 16, h + 16), layer2);
     this->layer3 = new MapLayer(sf::Vector2i(w + 16, h + 16), layer3);
-    this->layer1->move(-32, 0);
-    this->layer2->move(-32, 0);
-    this->layer3->move(-32, 0);
     this->bg = bg;
     if(!bg->getLoop()) {
         bg->setLoop(true);
