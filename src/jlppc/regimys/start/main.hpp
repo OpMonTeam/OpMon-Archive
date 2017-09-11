dc7c44bbc46739fb368b58ab1111251c34f9ae9c
Jlppc <jlppcprog@ntymail.com>
2017-09-11 13:20:09 +0200

    Code rereading : main

--- src/jlppc/regimys/start/main.hpp
+++ src/jlppc/regimys/start/main.hpp
@@ -7,6 +7,10 @@ http://opmon-game.ga
 #ifndef MAIN_HPP
 #define MAIN_HPP
 
+#ifndef __cplusplus
+#error This is a C++ program!
+#endif
+
 #include <iostream>
 #include <fstream>
 #include "../../utils/File.hpp"
@@ -14,19 +18,30 @@ http://opmon-game.ga
 #include <sstream>
 #include <SFML/System.hpp>
 
-
+/**
+Shortcuts
+*/
 #define UNS using namespace std;
 #define toStr(toStrP) #toStrP
 
+/**
+Equivalent of for(var : array) but with an iterator and a pointer.
+*/
 #define FOR_EACH(arrType, arrName, sizeArr, bracket)			\
   for(int itor = 0; itor < (sizeArr); itor++) bracket			\
       arrType* currentObj = NULL;				       	\
       currentObj = &(arrName[itor]);
-
+/**
+Linux only. If defined, the program will search for the resources on the local directory, instead of /usr/share/OpMon
+*/
 #define LOCAL_TEST
-
+/**
+Return how many time in milliseconds the program is open
+*/
 #define GET_TICKS ticks.getElapsedTime().asMilliseconds()
-
+/*
+This code define the location of the logs, saves and resources folders.
+*/
 #ifndef _WIN32
 
 #ifdef LOCAL_TEST
@@ -50,7 +65,9 @@ http://opmon-game.ga
 #define SAVE_PATH std::string("saves\\")
 
 #endif // _WIN32
-
+/**
+Use in a stream with the << operator. Prints the number of milliseconds passed since the beginning of the game.
+*/
 #define PRINT_TICKS "[T = " << ticks.getElapsedTime().asMilliseconds() << "] - "
 
 /**Principal log*/
@@ -59,37 +76,58 @@ extern std::ofstream rlog;
 extern std::ofstream rerrLog;
 /**Path to the options save file*/
 extern std::string optSave;
-
+/**
+sf::Clock counting the number of milliseconds passed since the beginning of the program
+*/
 extern sf::Clock ticks;
 
 //#define TEST
-
+/**
+Function that handles errors on the program
+*/
 void handleError(std::string const& errorName, bool fatal);
-
+/**
+Function quitting the game, deleting the resources
+*/
 int quit(int const& returns);
 
 class MainFrame;
-
+/**
+Namespace containing the main contents of the game
+*/
 namespace Main {
-void main();
-//->Useless
-extern long startTime;
-//  extern std::string sep;
-// extern bool reboot;
-extern Player player;
-
-extern MainFrame mainframe;
+  /**
+     The game's main
+  */
+  void main();
+  //->Useless (I should delete this variable, but I need to check some things first)
+  extern long startTime;
+  //  extern std::string sep;
+  // extern bool reboot;
+  /**
+     Contains the player
+  */
+  extern Player player;
+  /**
+     Contains the frame, and all the elements
+  */
+  extern MainFrame mainframe;
 
 }
 
 
-
+/**
+Don't you know what this is?
+*/
 int main(int argc, char *argv[]);
 
+/*
+Useful operators for strings
+*/
 std::string& operator<<(std::string &str, std::string const& thing);
 std::string& operator<<(std::string &str, int const& nbre);
 std::string& operator<<(std::string &str, char nbre[]);
-
+//->Unused (I don't know why.)
 typedef unsigned int p_int;
 
 
