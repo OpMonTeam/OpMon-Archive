e4b9775947a29c1567b81ce5ca6c65960cfb9a27
Navet56 <evandib@gmail.com>
2017-10-09 13:34:58 +0200

    Add the "Tiled Map" Tileset

---
+++ exe/resources/maps/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset" tilewidth="32" tileheight="32" tilecount="4096" columns="64">
+ <image source="tileset.png" width="2048" height="2048"/>
+</tileset>
