e8301654329674802a71677b4765c9aba15bf2cb
Jlppc <jlppcprog@ntymail.com>
2017-10-11 15:50:48 +0200

    Add Initializer variables for the battle

--- src/jlppc/regimys/start/Initializer.hpp
+++ src/jlppc/regimys/start/Initializer.hpp
@@ -32,7 +32,16 @@ extern std::vector<sf::Music *> townMusics;
 extern sf::Texture texturePP[4];
 extern sf::Texture walkingPP[4];
 extern sf::Texture walkingPP2[4];
+extern sf::Texture backPP;//Not init yet
 extern sf::Texture mapsFe[3];
+
+//Battle
+extern sf::Texture bActButtons[4];
+extern sf::Texture bHpBar;
+extern sf::Texture bHpBg;
+extern std::vector<sf::Texture> battleBackgrounds;
+
+extern std::vector<sf::Texture> charaFront;
 
 extern std::vector<std::vector<sf::Texture> > doorsTextures;
 //std::string backgrounds[];
