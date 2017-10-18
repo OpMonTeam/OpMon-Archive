85691ad25a207aac05558f2605a16206a3b7bbcb
Kévin Bernard-Allies <kevin.bernard.allies@gmail.com>
2017-10-18 17:50:00 +0200

    Simplify the *_PATH constants
    All path are declared in UNIX format (with "/" separator).
    `mkdir()` has been added the support of UNIX format under Windows.

--- src/regimys/start/main.cpp
+++ src/regimys/start/main.cpp
@@ -49,19 +49,22 @@ string getPath(string const& path) {
 /**
  * Make a new directory (if it does not exist yet).
  *
- * @param path
+ * @param path file path in the UNIX format
  * @return `true` if the directory exists; `false` in case of error.
  */
 bool mkdir(const std::string &path) {
+    // Path in the local system format
+    std::string path2 = getPath(path);
+
 #ifndef _WIN32
     const char *cmd = "mkdir -p ";
 #else
     const char *cmd = "mkdir ";
 #endif
 
-    int result = system((cmd + path).c_str());
+    int result = system((cmd + path2).c_str());
     if (result) {
-        cout << "creation of folder \"" << path << " failed: errno " << result << endl;
+        cout << "creation of folder \"" << path2 << " failed: errno " << result << endl;
     }
     #ifndef _WIN32
     return !result;
