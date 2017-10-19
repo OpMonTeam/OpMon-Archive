7ce4dcfc64c33aeebf5c05fde0221fe1fc40d9eb
Navet56 <evandib@gmail.com>
2017-10-19 20:26:14 +0200

    Fix data folder bug

---
+++ installers/Debian/source/usr/src/opmon/src/jlppc/regimys/enums/Nature.hpp
@@ -0,0 +1,64 @@
+/*
+Nature.hpp
+Author : Jlppc
+Fichier sous licence GPL-3.0
+http://opmon-game.ga
+Définit l'énumération des caractères
+*/
+#ifndef SRCCPP_JLPPC_REGIMYS_ENUMS_CARACTERE_HPP_
+#define SRCCPP_JLPPC_REGIMYS_ENUMS_CARACTERE_HPP_
+
+#include "Enums.hpp"
+/**
+Définit la classe des caractères
+*/
+//->EnumClass->Nature
+class NatureClass {
+public:
+    /**La stat que boost le caractère*/
+    //->ExpectEnum->Stats
+    int bonus;
+    /**La stat que réduit le caractère*/
+    //->ExpectEnum->Stats
+    int malus;
+
+    int id;
+    NatureClass(int bonus, int malus, int id) : bonus(bonus), malus(malus), id(id) {};
+    //->DontUse
+    NatureClass() {};
+
+};
+/**
+Enumère les caractères
+*/
+//->Enum
+namespace Nature {
+const NatureClass ASSURE = NatureClass(Stats::DEF, Stats::ATK, 0),
+                  BIZARRE = NatureClass(Stats::NOTHING, Stats::NOTHING, 1),
+                  BRAVE = NatureClass(Stats::ATK, Stats::SPE, 2),
+                  CALME = NatureClass(Stats::DEFSPE, Stats::ATK, 3),
+                  DISCRET = NatureClass(Stats::ATKSPE, Stats::SPE, 4),
+                  DOCILE = NatureClass(Stats::NOTHING, Stats::NOTHING, 5),
+                  DOUX = NatureClass(Stats::ATKSPE, Stats::DEF, 6),
+                  FOUFOU = NatureClass(Stats::ATKSPE, Stats::DEFSPE, 7),
+                  GENTIL = NatureClass(Stats::DEFSPE, Stats::DEF, 8),
+                  HARDI = NatureClass(Stats::NOTHING, Stats::NOTHING, 9),
+                  JOVIAL = NatureClass(Stats::SPE, Stats::ATKSPE, 10),
+                  LACHE = NatureClass(Stats::DEF, Stats::DEFSPE, 11),
+                  MALIN = NatureClass(Stats::DEF, Stats::ATKSPE, 12),
+                  MALPOLI = NatureClass(Stats::DEFSPE, Stats::SPE, 13),
+                  MAUVAIS = NatureClass(Stats::ATK, Stats::DEFSPE, 14),
+                  MODESTE = NatureClass(Stats::ATKSPE, Stats::ATK, 15),
+                  NAIF = NatureClass(Stats::SPE, Stats::DEFSPE, 16),
+                  PRESSE = NatureClass(Stats::SPE, Stats::DEF, 17),
+                  PRUDENT = NatureClass(Stats::DEFSPE, Stats::ATKSPE, 18),
+                  PUDIQUE = NatureClass(Stats::NOTHING, Stats::NOTHING, 19),
+                  RELAX = NatureClass(Stats::DEF, Stats::SPE, 20),
+                  RIGIDE = NatureClass(Stats::ATK, Stats::ATKSPE, 21),
+                  SERIEUX = NatureClass(Stats::NOTHING, Stats::NOTHING, 22),
+                  SOLO = NatureClass(Stats::ATK, Stats::DEF, 23),
+                  TIMIDE = NatureClass(Stats::SPE, Stats::ATK, 24);
+extern const NatureClass *enumsList[25];
+};
+
+#endif /* SRCCPP_JLPPC_REGIMYS_ENUMS_CARACTERE_HPP_ */
---
+++ installers/Debian/source/usr/src/opmon/src/jlppc/regimys/objects/Attack.hpp
@@ -0,0 +1,73 @@
+/*
+Attack.hpp
+Author : Jlppc
+Fichier sous licence GPL-3.0
+http://opmon-game.ga
+Contient la définition de la classe Attack
+*/
+#ifndef SRCCPP_JLPPC_REGIMYS_OBJECTS_ATTAQUE_HPP_
+#define SRCCPP_JLPPC_REGIMYS_OBJECTS_ATTAQUE_HPP_
+
+#include <iostream>
+#include "../../utils/Utils.hpp"
+#include "../enums/Enums.hpp"
+#include "../../utils/Class.hpp"
+
+class OpMon;
+/**
+Représente une attaque Pokémon
+*/
+//->PureVirtual
+class Attack {
+public:
+    virtual ~Attack() {}
+    Attack(std::string nom, int puissance, int type, int accuracy, bool special, bool status, int chanceDeCoups, bool rateJamais, int ppMax, int priorite, std::string className);
+    //->PureVirtual
+    virtual int effetAvant(OpMon &atk, OpMon &def) = 0;
+    //->PureVirtual
+    virtual int effetApres(OpMon &atk, OpMon &def) = 0;
+    void healPP() {
+        pp = ppMax;
+    }
+    int getType() {
+        return type;
+    }
+    /**atk attaque le pokémon def*/
+    //->Final
+    virtual int attack(OpMon &atk, OpMon &def);
+    virtual void siEchoue(OpMon &atk, OpMon &def) {}
+    virtual Class<Attack> *getClass() {
+        return classe;
+    }
+    virtual std::string getClassName() {
+        return className;
+    }
+    virtual std::string save();
+    void setPP(int PP) {
+        this->pp = PP;
+    }
+    void setPPMax(int PPMax) {
+        this->ppMax = PPMax;
+    }
+protected:
+    std::string className;
+    std::string nom;
+    int puissance;
+    int priorite;
+    int accuracy;
+    int type;
+    bool special;
+    bool status;
+    int part = 0;
+    int pp;
+    int ppMax;
+    /**La chance de coups critiques, en 1/nombre (Par défaut, 16)*/
+    int chanceDeCoups;
+    bool rateJamais;
+    /**Variable utilisée dans effetAvant et effetAprès*/
+    int hpPerdus = 0;
+private:
+    static Class<Attack> *classe;
+};
+
+#endif /* SRCCPP_JLPPC_REGIMYS_OBJECTS_ATTAQUE_HPP_ */
