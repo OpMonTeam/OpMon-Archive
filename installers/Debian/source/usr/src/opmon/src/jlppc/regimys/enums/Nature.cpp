c2420454039c1339dfa6073cb595b9f9a6a33f4d
Navet56 <evandib@gmail.com>
2017-10-19 20:26:14 +0200

    Fix data folder bug

---
+++ installers/Debian/source/usr/src/opmon/src/jlppc/regimys/enums/Nature.cpp
@@ -0,0 +1,7 @@
+#include "Nature.hpp"
+namespace Nature {
+const NatureClass *enumsList[25] = {&ASSURE, &BIZARRE, &BRAVE, &CALME, &DISCRET, &DOCILE, &DOUX, &FOUFOU, &GENTIL, &HARDI, &JOVIAL, &LACHE, &MALIN, &MALPOLI, &MAUVAIS, &MODESTE,
+                                    &NAIF, &PRESSE, &PRUDENT, &PUDIQUE, &RELAX, &RIGIDE, &SERIEUX/*Why so serious?*/,&SOLO, &TIMIDE
+                                   };
+}
+
