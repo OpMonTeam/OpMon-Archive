4471b0934b83fa7edc1212bda91254dfdc1aec99
Jlppc <jlppcprog@ntymail.com>
2017-10-31 13:15:32 +0100

    Separating main in two parts main : The functions to start the program core : The core function of the program

---
+++ newSrc/opmon/start/Core.hpp
@@ -0,0 +1,48 @@
+/**
+   Core.hpp : Contains the main function and variables of the program.
+ */
+#ifndef CORE_HPP
+#define CORE_HPP
+
+#include <iostream>
+#include <SFML/System/Clock.hpp>
+
+/**Path to the options save file*/
+extern std::string optSave;
+/**
+sf::Clock counting the number of milliseconds passed since the beginning of the program
+*/
+extern sf::Clock ticks;
+/**
+Returns the path depending of the system.
+By default, please use the "/" separator.
+Then, if the os is Windows, this function will change the "/" by "\".
+If you use the "\" separator in the path given,
+nothing will change, on unix systems and on Windows systems.
+*/
+std::string getPath(std::string const& path);
+
+//#define TEST
+/**
+Function that handles errors on the program
+*/
+void handleError(std::string const& errorName, bool fatal);
+/**
+Function quitting the game, deleting the resources
+*/
+int quit(int const& returns);
+
+
+// Create a directory (if it doesn't exist yet)
+bool mkdir(const std::string &path);
+
+
+/*
+Useful operators for strings
+*/
+std::string& operator<<(std::string &str, std::string const& thing);
+std::string& operator<<(std::string &str, int const& nbre);
+std::string& operator<<(std::string &str, char nbre[]);
+
+
+#endif
---
+++ newSrc/utils/StringKeys.hpp
@@ -0,0 +1,27 @@
+/*
+StringKeys.hpp
+Author : Jlppc
+File under GNU GPL 3.0 licence
+http://opmon-game.ga
+*/
+#ifndef STRINGKEYS_HPP
+#define STRINGKEYS_HPP
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <SFML/System.hpp>
+#include "main.hpp"
+
+namespace StringKeys {
+extern std::vector<std::string> keys;
+extern std::vector<sf::String> strings;
+sf::String &get(std::string key);
+void initialize(std::string file);
+OP_DEPRECATED sf::String split(sf::String const& str, char const& splitter, int const& part);
+std::vector<sf::String> split(sf::String const& str, char const& splitter);
+std::string sfStringtoStdString(sf::String const &str);
+int countInstances(sf::String const& str, char const& toSearch);
+}
+
+#endif // STRINGKEYS_HPP
