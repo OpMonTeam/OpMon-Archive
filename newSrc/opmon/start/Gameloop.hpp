b309f00ec2e4fb4bc66078e87f96ba0b06461139
jlppc <jlppcprog@ntymail.com>
2017-11-03 16:26:29 +0100

    Add GameLoop's base

--- newSrc/opmon/start/Gameloop.hpp
+++ newSrc/opmon/start/Gameloop.hpp
@@ -1,12 +1,26 @@
 #ifndef GAMELOOP_HPP
 #define GAMELOOP_HPP
 
+#define FPS_TICKS 33
+
 namespace OpMon{
   class GameLoop{
   public:
     GameLoop();
     GameStatus operator()();
     GameStatus checkQuit();
+
+    void setActualDialog(std::vector<sf::String> newDialog){
+      actualDialog = newDialog;
+    }
+
+    bool isWait(){
+      return wait;
+    }
+
+    void setWait(bool wait){
+      this->wait = wait;
+    }
     
   private:
     sf::Event events;
@@ -16,8 +30,12 @@ namespace OpMon{
     View::OptionsMenu optionsmenu;
     GamePart gamepart = GamePart::START;
     bool endGame = false;
+    bool wait = true;
 
+    std::vector<sf::String> actualDialog;
+    
     int frames = 0;
+    int oldticks = 0;
   }
 }
 
