fe1c8c7e9db9d0b3f01078c02b06f57c049539c4
Jlppc <jlppcprog@ntymail.com>
2017-12-12 11:02:22 +0100

    Add Menu controller

---
+++ newSrc/opmon/controller/MenuCtrl.hpp
@@ -0,0 +1,12 @@
+#ifndef MENUCTRL_HPP
+#define MENUCTRL_HPP
+
+namespace OpMon{
+	namespace Controller{
+		namespace MenuCtrl{
+			void checkEvents(sf::Events& event, View::MainMenu& menu);
+		}
+	}
+}
+
+#endif
