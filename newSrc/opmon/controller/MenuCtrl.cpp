e695553b853ff56ab8b64f504b2e9347ac11c9bf
Jlppc <jlppcprog@ntymail.com>
2017-12-15 08:54:23 +0100

    Started to add Interfaces stack in GameLoop

--- newSrc/opmon/controller/MenuCtrl.cpp
+++ newSrc/opmon/controller/MenuCtrl.cpp
@@ -1,38 +1,38 @@
 #include "MenuCtrl.hpp"
 
 namespace OpMon{
-	namespace Controller{
-		namespace MenuCtrl{
-			Interface* chackEvents(sf::Events& event, View::MainMenu& menu){
-				switch(event) {
+  namespace Controller{
+    namespace MenuCtrl{
+      Interface* checkEvents(sf::Events& event, View::MainMenu& menu){
+	switch(event) {
+	  
+	case sf::Event::KeyPressed:
+	  if(event.key.code == sf::Keyboard::Return) {
+	    switch(curPosI) {
+	    case 0:
+	      bgMusTitle.stop();
+	      return (View::Interface*) new View::StartScene();
+	    case 3:
+	      bgMusTitle.stop();
+	      return nullptr;
+	    case 2:
+	      return (View::Interface*) new View::OptionsMenu();
+	      break;
+	    case 1:
+	      Model::Data::Sounds::nopeSound.play();
+	      return &menu;
+	    }
+	  }else if(event.key.code == sf::Keyboard::Up){
+	    menu.moveArrow(true);
+	  }else if(event.key.code == sf::Keyboard::Down){
+	    menu.moveArrow(false);
+	  }
+	  break;
+	default:
+	  break;
 
-				  case sf::Event::KeyPressed:
-					if(event.key.code == sf::Keyboard::Return) {
-					  switch(curPosI) {
-					  case 0:
-						bgMusTitle.stop();
-						return (View::Interface*) new View::StartScene();
-					  case 3:
-						bgMusTitle.stop();
-						return nullptr;
-					  case 2:
-						return (View::Interface*) new View::OptionsMenu();
-						break;
-					  case 1:
-						Model::Data::Sounds::nopeSound.play();
-						return &menu;
-					  }
-					}else if(event.key.code == sf::Keyboard::Up){
-						menu.moveArrow(true);
-					}else if(event.key.code == sf::Keyboard::Down){
-						menu.moveArrow(false);
-					}
-					break;
-				default:
-					break;
-
-				}
-			}
-		}
 	}
+      }
+    }
+  }
 }
