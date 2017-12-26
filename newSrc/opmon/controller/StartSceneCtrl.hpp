887e790c7aea542ef92380b8dea7a69fd132ec36
jlppc <jlppcprog@ntymail.com>
2017-12-26 15:43:29 +0100

    Added StartScene's controller

---
+++ newSrc/opmon/controller/StartSceneCtrl.hpp
@@ -0,0 +1,12 @@
+#ifndef STARTSCENECTRL_HPP
+#define STARTSCENECTRL_HPP
+
+namespace OpMon{
+  namespace Controller{
+    namespace StartSceneCtrl{
+      GameStatus checkEvents(Event& event, StartScene& startscene);
+    }
+  }
+}
+
+#endif
