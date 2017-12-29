6b3a18585360238e04d26b1046311b524215312f
jlppc <jlppcprog@ntymail.com>
2017-12-29 15:32:14 +0100

    newSrc -> src | src -> oldSrc

--- oldSrc/utils/Utils.hpp
+++ oldSrc/utils/Utils.hpp
@@ -8,6 +8,8 @@ Définit le namespace Utils
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
+#include <ctime>
+#include <cstdlib>
 #include <iostream>
 /**
 Namespace contenant des fonctions utiles
@@ -17,8 +19,6 @@ int randU(int limit);
 unsigned int randUI(unsigned int limit);
 void wait(int ticks);
 long hash(std::string str);
-  // Create a directory (if it doesn't exist yet)
-bool mkdir(const std::string &path);
 }
 
 #endif // UTILS_HPP
