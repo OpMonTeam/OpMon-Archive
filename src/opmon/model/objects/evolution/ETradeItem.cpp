3b264a561142fcc0208ca97e61866f149abc7ca3
jlppc <jlppcprog@ntymail.com>
2017-12-31 13:20:40 +0100

    Add evolutions

---
+++ src/opmon/model/objects/evolution/ETradeItem.cpp
@@ -0,0 +1,17 @@
+#include "evolutions.hpp"
+
+namespace OpMon{
+  namespace Model{
+    
+    namespace Evolutions {
+      bool E_TradeItem::checkEvolve(OpMon const &toCheck) {
+	if (forEvolve == nullptr) {
+	  return false;
+	}
+	Item *held = toCheck.itemHeld();
+	return held == (forEvolve);
+      }
+    }
+
+  }
+}
