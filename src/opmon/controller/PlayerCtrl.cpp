9f349e62c64be81ae743ffb8e7af4d6fbbd8bbef
Jlppc <jlppcprog@ntymail.com>
2018-01-03 11:51:19 +0100

    Fixed compilation errors (PlayerCtrl.hpp)

--- src/opmon/controller/PlayerCtrl.cpp
+++ src/opmon/controller/PlayerCtrl.cpp
@@ -1,47 +1,50 @@
 #include "PlayerCtrl.hpp"
 
+#include "EventsCtrl.hpp"
+#include "../model/storage/Data.hpp"
+
 namespace OpMon{
   namespace Controller{
-    void PlayerCtrl::checkMove(Player& player, sf::Event const& events, View::Overworld const& overworld){
-      switch(events){
+    void PlayerCtrl::checkMove(Model::Player& player, sf::Event const& events, View::Overworld const& overworld){
+      switch(events.type){
       case sf::Event::KeyPressed:
 	if(events.key.code == sf::Keyboard::F5) {
-	    player.tp("Mom's room", sf::Vector2i(0, 1), true);
-	  } else if(events.key.code == sf::Keyboard::F6) {
-	    player.tp("Player's room", sf::Vector2i(0, 0), true);
-	  } else if(events.key.code == sf::Keyboard::F1) {
-	    player.tp("Fauxbourg Euvi", sf::Vector2i(25, 28), true);
-	  } else if(events.key.code == sf::Keyboard::F2) {
-	    player.tp("Player's home", sf::Vector2i(8, 14), true);
-	  } else if(events.key.code == sf::Keyboard::F3) {
-	    player.tp("Laboratory", sf::Vector2i(15, 14), true);
-	  } else if(events.key.code == sf::Keyboard::F4) {
-	    player.tp("Rival's house", sf::Vector2i(8, 14), true);
-	  } else if(events.key.code == sf::Keyboard::F7){
-	    player.tp("Route 14", sf::Vector2i(0, 31), true);
-	  } else if(events.key.code == sf::Keyboard::F8){
-	    player.tp("MysteriouCity", sf::Vector2i(12, 0), true);
-	  }
+	  player.tp("Mom's room", sf::Vector2i(0, 1));
+	} else if(events.key.code == sf::Keyboard::F6) {
+	  player.tp("Player's room", sf::Vector2i(0, 0));
+	} else if(events.key.code == sf::Keyboard::F1) {
+	  player.tp("Fauxbourg Euvi", sf::Vector2i(25, 28));
+	} else if(events.key.code == sf::Keyboard::F2) {
+	  player.tp("Player's home", sf::Vector2i(8, 14));
+	} else if(events.key.code == sf::Keyboard::F3) {
+	  player.tp("Laboratory", sf::Vector2i(15, 14));
+	} else if(events.key.code == sf::Keyboard::F4) {
+	  player.tp("Rival's house", sf::Vector2i(8, 14));
+	} else if(events.key.code == sf::Keyboard::F7){
+	  player.tp("Route 14", sf::Vector2i(0, 31));
+	} else if(events.key.code == sf::Keyboard::F8){
+	  player.tp("MysteriouCity", sf::Vector2i(12, 0));
+	}
 	break;
       }
       if(!overworld.justTp) {
-	if(isKeyPressed(sf::Keyboard::Up)) {
-	  gameloop.getPlayer().getPosition().move(Side::TO_UP);
-	} else if(isKeyPressed(sf::Keyboard::Down)) {
-	  gameloop.getPlayer().getPosition().move(Side::TO_DOWN);
-	} else if(isKeyPressed(sf::Keyboard::Left)) {
-	  gameloop.getPlayer().getPosition().move(Side::TO_LEFT);
-	} else if(isKeyPressed(sf::Keyboard::Right)) {
-	  gameloop.getPlayer().getPosition().move(Side::TO_RIGHT);
+	if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+	  gameloop.getPlayer().getPosition().move(Model::Side::TO_UP);
+	} else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+	  gameloop.getPlayer().getPosition().move(Model::Side::TO_DOWN);
+	} else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+	  gameloop.getPlayer().getPosition().move(Model::Side::TO_LEFT);
+	} else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+	  gameloop.getPlayer().getPosition().move(Model::Side::TO_RIGHT);
 	}
       }
 
       
     }
 
-    void move(Side direction, Player& player){
+    void move(Model::Side direction, Model::Player& player){
       player.getPosition().move(direction);
-      EventsCtrl::actionEvent(Model::Data::World::maps.at(player.getPosition().getMapId()).getEvent(gameloop.getPlayer().getPosition().getPositionPixel()), player, Events::EventTrigger::GO_IN);
+      EventsCtrl::actionEvents(Model::Data::World::maps.at(player.getMapId())->getEvent(gameloop.getPlayer().getPosition().getPositionPixel()), player, Model::Events::EventTrigger::GO_IN);
 
     }
   }
