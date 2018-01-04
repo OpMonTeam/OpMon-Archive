86b154bbef91d8b808701bf0bf825784c23c082b
Kévin Bernard-Allies <kevin.bernard.allies@gmail.com>
2018-01-04 19:38:05 +0100

    Fix a few compilation errors (mostly missing headers)

--- src/opmon/controller/StartSceneCtrl.hpp
+++ src/opmon/controller/StartSceneCtrl.hpp
@@ -1,10 +1,15 @@
 #ifndef STARTSCENECTRL_HPP
 #define STARTSCENECTRL_HPP
 
+#include <SFML/Window.hpp>
+#include "../start/Core.hpp"
+#include "../view/StartScene.hpp"
+
+
 namespace OpMon{
   namespace Controller{
     namespace StartSceneCtrl{
-      GameStatus checkEvents(Event& event, StartScene& startscene);
+      GameStatus checkEvents(sf::Event& event, OpMon::View::StartScene& startscene);
     }
   }
 }
