31245e3af3a491f19f1dc8a31e03e5852bf63f6b
jlppc <jlppcprog@ntymail.com>
2018-01-14 08:00:38 +0100

    Fixing some things in Event Apparently I forgot to do some major things with Event. I forgot to add the new Position class for the events, so I will add it in the next commits.

--- src/opmon/model/sysObjects/Position.cpp
+++ src/opmon/model/sysObjects/Position.cpp
@@ -5,11 +5,11 @@
 
 namespace OpMon {
     namespace Model {
-        Position::Position()
-                : mapId("Player's room"), posX(0), posY(0), dir(Side::TO_UP), anim(false), movement(false) {}
+      Position::Position(bool event = false)
+	  : mapId("Player's room"), posX(0), posY(0), dir(Side::TO_UP), anim(false), movement(false), event(event) {}
 
-        Position::Position(std::string mapId, sf::Vector2i position, Side dir = Side::TO_UP)
-                : mapId(mapId), posX(position.x), posY(position.y), dir(dir), anim(false), movement(false) {}
+      Position::Position(std::string mapId, sf::Vector2i position, Side dir = Side::TO_UP, bool event = false)
+	  : mapId(mapId), posX(position.x), posY(position.y), dir(dir), anim(false), movement(false), event(event) {}
 
         void Position::tp(sf::Vector2i position) {
           if (!movement || !anim) {
