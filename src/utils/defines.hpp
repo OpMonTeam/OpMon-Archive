dc7e448717f7e2bc6dece564481283f38748b394
Kévin Bernard-Allies <kevin.bernard.allies@gmail.com>
2018-01-23 21:33:18 +0100

    Use dynamic resource path detection instead of hard-coded path

--- src/utils/defines.hpp
+++ src/utils/defines.hpp
@@ -16,19 +16,6 @@ Shortcuts
 */
 #define UNS using namespace std;
 
-/**
-Linux only. If defined, the program will search for the resources on the local directory, instead of /usr/share/OpMon
-*/
-#define LOCAL_TEST
-
-/**
- * Location of the resource folder
- */
-#if defined(LOCAL_TEST) || defined(_WIN32)
-#define RESSOURCES_PATH std::string("data/")
-#else
-#define RESSOURCES_PATH std::string("/usr/share/OpMon/")
-#endif
 
 /* location of the save folder */
 #define SAVE_PATH std::string("saves/")
