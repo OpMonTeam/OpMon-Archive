f3dba4e4a4d2ea329defc144fac547f4d5ef3b97
Kévin Bernard-Allies <kevin.bernard.allies@gmail.com>
2018-01-24 23:07:10 +0100

    Add SFML 2.3 compatibility

--- src/opmon/view/Window.cpp
+++ src/opmon/view/Window.cpp
@@ -49,7 +49,7 @@ namespace OpMon{
       textLoad.setString(Utils::StringKeys::get("load.txt"));
       textLoad.setCharacterSize(45);
       textLoad.setFont(Model::Data::Ui::font);
-      textLoad.setFillColor(sf::Color::White);
+      textLoad.setColor(sf::Color::White);
       textLoad.setPosition(30, 400);
       frame.draw(spriteLoad);
       frame.draw(textLoad);
