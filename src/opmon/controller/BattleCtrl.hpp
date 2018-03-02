5a4caea6f892febdc969108d2b74a4d724634463
Cyrion <jlppcprog@ntymail.com>
2018-03-02 09:21:32 +0100

    Add two OpMon battle initialization

--- src/opmon/controller/BattleCtrl.hpp
+++ src/opmon/controller/BattleCtrl.hpp
@@ -4,6 +4,10 @@
 #include "../view/Battle.hpp"
 #include "../model/sysObjects/OpTeam.hpp"
 #include "AGameScreen.hpp"
+#include <vector>
+#include "../model/objects/Enums.hpp"
+#include "../model/objects/Attack.hpp"
+#include "../model/objects/Species.hpp"
 
 namespace OpMon{
   namespace Controller{
@@ -18,16 +22,47 @@ namespace OpMon{
       Model::OpTeam* playerTeam;
       Model::OpTeam* trainerTeam;
       
-      View::Battle battle;
+      View::Battle view;
 
       View::Turn* atkTurn;
       View::Turn* defTurn;
+
+      int oldStats[2][5];
+      Model::Type oldTypes[2][2];
+      Model::Speces oldSpecies[2];
+      std::vector<Model::Attack *> oldAttacks[2];
+      
+      int trainerOpCount = 0;
+
+      bool turnActivated = false;
+      
+      /** Counters and triggers for the fight*/
+      bool sameAtk = false;
+      bool sameDef = false;
+      bool atkCanAttack = true;
+      bool defCanAttack = true;
+      int atkSleepCD = 0;
+      bool atkSleep = false;
+      int defSleepCD = 0;
+      bool defSleep = false;
+      int atkConfusedCD = 0;
+      bool atkConfued = false;
+      int defConfusedCD = 0;
+      bool defConfused;
+      int atkAttack = 0;
+      int defAttack = 0;
+      bool captured = false;
+      
+
+      bool turn();
+      void initBattle();
+      View::Turn* turnIA(int level);
+
       
     public:
       BattleCtrl(Model::OpTeam* one, Model::OpTeam *two);
       GameStatus checkEvent(sf::Event const &) override;
       GameStatus update() override;
-      bool turn();
     };
 
   }
