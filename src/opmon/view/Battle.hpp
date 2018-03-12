47e8f651279d445f3ff613ec6adb1ed4d3dd3dce
Jlppc <jlppcprog@ntymail.com>
2018-03-12 17:35:18 +0100

    Commented some code for the contributors

--- src/opmon/view/Battle.hpp
+++ src/opmon/view/Battle.hpp
@@ -13,17 +13,22 @@ namespace OpMon{
        
     class Battle {
     private:
+      //The OpMons' sprites (The textures IDs will be in the OpMon object)
       sf::Sprite atk;
       sf::Sprite def;
 
+      //The cursor's position for the choices
       int cursorPos;
+      //The list of cursor's positions in pixel, depending of the situation
       std::map<std::string, std::vector<sf::Vector2f> > cursorsList;
-      
+      //True if the turn have been calculated
       bool turnLaunched = false;
     public:
+      //Used to print the turn
       GameStatus operator()(Model::Turn atk, Model::Turn def);//Sets turnLaunched to true
+      //Used to print the choices
       GameStatus operator()(Model::Turn* atkTurn);
-
+      //Moves the cursor
       void moveCur(Model::Side where);
       
     };
