28a336c9328e5159f25321b64155f7d56e23caf7
Jlppc <jlppcprog@ntymail.com>
2018-03-29 16:06:39 +0200

    Added Battle's constructor and some variables

--- src/opmon/view/Battle.cpp
+++ src/opmon/view/Battle.cpp
@@ -1,4 +1,5 @@
 #include "Battle.hpp"
+#include "../model/sysObjects/OpTeam.hpp"
 
 namespace OpMon{
   namespace View{
@@ -8,6 +9,10 @@ namespace OpMon{
 
     GameStatus Battle::operator()(Model::Turn const& atk, Model::Turn const& def){
       return GameStatus::CONTINUE;
+    }
+
+    Battle::Battle(const Model::OpTeam* atkTeam, const Model::OpTeam* defTeam, std::string trainerClass, std::string background)
+      : atkTeam(atkTeam), defTeam(defTeam){
     }
   }
 }
