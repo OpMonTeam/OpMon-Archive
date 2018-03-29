1b694681c8593a389fc2b4f58cb863a6ddc308e4
Jlppc <jlppcprog@ntymail.com>
2018-03-29 15:37:41 +0200

    Fixed compilation errors for the battle

--- src/opmon/view/Battle.cpp
+++ src/opmon/view/Battle.cpp
@@ -2,6 +2,12 @@
 
 namespace OpMon{
   namespace View{
-    
+    GameStatus Battle::operator()(Model::Turn* atkTurn){
+      return GameStatus::CONTINUE;
+    }
+
+    GameStatus Battle::operator()(Model::Turn const& atk, Model::Turn const& def){
+      return GameStatus::CONTINUE;
+    }
   }
 }
