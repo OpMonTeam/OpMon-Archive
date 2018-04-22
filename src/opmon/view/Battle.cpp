461b9ea32567bb183c12e9537b37a9cbd12a0fda
Navet56 <evandib@gmail.com>
2018-04-22 12:00:24 +0200

    Update positions

--- src/opmon/view/Battle.cpp
+++ src/opmon/view/Battle.cpp
@@ -28,25 +28,25 @@ namespace OpMon{
       : atkTeam(atkTeam), defTeam(defTeam){
       this->background.setTexture(Model::Data::Battle::backgrounds[background]);
       playerSpr.setTexture(Model::Data::Battle::charaBattleTextures["player"][0]);
-      playerSpr.setPosition(0, 40);
-      playerSpr.setScale(2, 2);
+      playerSpr.setPosition(0, 193);
+      playerSpr.setScale(1, 1);
       trainerSpr.setTexture(Model::Data::Battle::charaBattleTextures[trainerClass][0]);
       trainerSpr.setPosition(400, 20);
       choicesTxt[0].setString(Utils::StringKeys::get("battle.attack"));
       choicesTxt[1].setString(Utils::StringKeys::get("battle.bag"));
       choicesTxt[2].setString(Utils::StringKeys::get("battle.opmon"));
       choicesTxt[3].setString(Utils::StringKeys::get("battle.run"));
-      posChoices[0].x = 250;
-      posChoices[0].y = 375;
-      posChoices[1].x = 400;
-      posChoices[1].y = 375;
-      posChoices[2].x = 250;
-      posChoices[2].y = 425;
-      posChoices[3].x = 400;
-      posChoices[3].y = 425;
+      posChoices[0].x = 280;
+      posChoices[0].y = 385;
+      posChoices[1].x = 420;
+      posChoices[1].y = 385;
+      posChoices[2].x = 280;
+      posChoices[2].y = 435;
+      posChoices[3].x = 420;
+      posChoices[3].y = 435;
       for(unsigned int i = 0; i < 4; i++){
 	choicesTxt[i].setFont(Model::Data::Ui::font);
-	choicesTxt[i].setCharacterSize(28);
+	choicesTxt[i].setCharacterSize(21);
 	choicesTxt[i].setPosition(posChoices[i]);
 	choicesTxt[i].setColor(sf::Color::Black);
       }
