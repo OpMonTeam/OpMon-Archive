888c070e2c5fbed74fb43dfa35ba65536feb16ea
Cyrion <jlppcprog@ntymail.com>
2018-04-29 13:25:48 +0200

    The opmons' data are now visible The name, the level and the health (The health bar is not functional)

--- src/opmon/view/Battle.cpp
+++ src/opmon/view/Battle.cpp
@@ -4,6 +4,7 @@
 #include "../../utils/StringKeys.hpp"
 #include "Window.hpp"
 #include <iostream>
+#include <sstream>
 
 namespace OpMon{
   namespace View{
@@ -15,6 +16,28 @@ namespace OpMon{
       this->def.setTexture(Model::Data::OpMons::opSprites[def->opmon->getSpecies().getOpdexNumber()][1]);
       frame.draw(atk);
       frame.draw(this->def);
+
+      for(unsigned int i = 0; i < 2; i++){
+	frame.draw(infobox[i]);
+	frame.draw(healthbar1[i]);
+	frame.draw(healthbar2[i]);
+      }
+
+      std::ostringstream oss;
+      oss << "Lv. " << atkTurn->opmon->getLevel();
+      opLevel[0].setString(oss.str());
+      std::ostringstream oss2;
+      oss2 << "Lv. " << def->opmon->getLevel();
+      opLevel[1].setString(oss2.str());
+
+      opName[0].setString(atkTurn->opmon->getNickname());
+      opName[1].setString(def->opmon->getNickname());
+
+      frame.draw(opName[0]);
+      frame.draw(opName[1]);
+      frame.draw(opLevel[0]);
+      frame.draw(opLevel[1]);
+      
       frame.draw(dialogSpr);
       for(sf::Text &txt : choicesTxt){
 	frame.draw(txt);
@@ -40,11 +63,11 @@ namespace OpMon{
       choicesTxt[1].setString(Utils::StringKeys::get("battle.bag"));
       choicesTxt[2].setString(Utils::StringKeys::get("battle.opmon"));
       choicesTxt[3].setString(Utils::StringKeys::get("battle.run"));
-      posChoices[0].x = 80;
+      posChoices[0].x = 60;
       posChoices[0].y = 385;
       posChoices[1].x = 220;
       posChoices[1].y = 385;
-      posChoices[2].x = 80;
+      posChoices[2].x = 60;
       posChoices[2].y = 435;
       posChoices[3].x = 220;
       posChoices[3].y = 435;
@@ -65,6 +88,35 @@ namespace OpMon{
       def.setPosition(305, 120);
       atk.setScale(2, 2);
 
+
+      for(unsigned int i = 0; i < 2; i++){
+	infobox[i].setTexture(Model::Data::Battle::infoBox);
+	healthbar1[i].setTexture(Model::Data::Battle::healthbar1);
+	healthbar2[i].setTexture(Model::Data::Battle::healthbar2);
+      }
+
+      infobox[0].setPosition(17, 148);
+      healthbar1[0].setPosition(30, 130);
+      healthbar2[0].setPosition(30, 130);
+
+      infobox[1].setPosition(321, 277);
+      healthbar1[1].setPosition(335, 257);
+      healthbar2[1].setPosition(335, 257);
+
+      opName[0].setPosition(22, 160);
+      opLevel[0].setPosition(22, 185);
+      opName[1].setPosition(332, 289);
+      opLevel[1].setPosition(332, 314);
+
+      for(unsigned int i = 0; i < 2; i++){
+	opName[i].setFont(Model::Data::Ui::font);
+	opLevel[i].setFont(Model::Data::Ui::font);
+	opName[i].setCharacterSize(22);
+	opLevel[i].setCharacterSize(22);
+	opName[i].setColor(sf::Color::Black);
+	opLevel[i].setColor(sf::Color::Black);
+      }
+      
     }
 
     void Battle::moveCur(Model::Side where){
