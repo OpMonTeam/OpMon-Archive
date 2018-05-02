1a66cc60d16f286abc02897d341901c2c4f3bbe6
Cyrion <jlppcprog@ntymail.com>
2018-05-02 13:01:05 +0200

    Fixed compilation errors

--- src/opmon/model/objects/item/Item.cpp
+++ src/opmon/model/objects/item/Item.cpp
@@ -22,21 +22,21 @@ UNS
 	//Jusqu'a 24 : Baies (Pas de baies de soin conditionnel, pas de baies autres
 	Item::itemsLst[25] = new I_Heal("Défense +", Stats::DEF, 25);
 	//Jusqu'a 31 : Stats+ sauf Muscle +
-	Item::itemsLst[32] = new Item("Améliorator", false, false, true, BagCat::OBJETS, 32);
+	Item::itemsLst[32] = new Item("Améliorator", false, false, true, BagCat::OBJECTS, 32);
 	//Jusqu'a 42 : Objets evolution / echange a tenir
-	Item::itemsLst[43] = new Item("Pierre Eclat", true, false, true, BagCat::OBJETS, 43);
+	Item::itemsLst[43] = new Item("Pierre Eclat", true, false, true, BagCat::OBJECTS, 43);
 	//Jusqu'a 51 : Pierres evolutives
-	Item::itemsLst[52] = new Item("Petit Champi", false, false, true, BagCat::OBJETS, 52);
+	Item::itemsLst[52] = new Item("Petit Champi", false, false, true, BagCat::OBJECTS, 52);
 	//Jusqu'a 71 : Items a vendre
 	//itemsLst[] = new const) Items d'amélioration d'EV. Classe a ajouter.
 	//Jusqu'a 82 : Items d'amélioration d'EV (Sauf : Aile Sprint, Pilule Talent, Aile Mental, Aile Esprit, Aile Santé)
-	Item::itemsLst[83] = new I_Heal("Soda Cool", true, true, true, BagCat::SOIN, 60, 83);
+	Item::itemsLst[83] = new I_Heal("Soda Cool", true, true, true, BagCat::HEAL, 60, 83);
 	//Jusqu'a 95 : Items de soin PV
-	Item::itemsLst[96] = new I_Heal("Huile", true, true, true, BagCat::SOIN, 10, false, 96);
+	Item::itemsLst[96] = new I_Heal("Huile", true, true, true, BagCat::HEAL, 10, false, 96);
 	//Jusqu'a 99 : Items de soin de PP
 	//itemsLst[] = new const I_Heal("Rappel", TODO)) Items de Rappel a faire
 	//Jusqu'a 103 : Items de Rappel
-	Item::itemsLst[104] = new I_Heal("Galette Illumis", true, true, true, BagCat::SOIN, 0, Status::POISONED, true, 104);
+	Item::itemsLst[104] = new I_Heal("Galette Illumis", true, true, true, BagCat::HEAL, 0, Status::POISONED, true, 104);
 	//Jusqu'a 114 : Items de Soin de Status
 	Item::itemsLst[115] = new I_OpBox("Poké Ball", 1, 115);
 	//Jusqu'a 124 : Balls classiques (+ Memoire Ball + HonorC:\Users\_le_pichon\Downloads\PPR\srcCpp\jlppc\regimys\objects\item\Item.cpp|66|error: expected type-specifier before 'I_Heal'| ball + Reve ball)
@@ -46,7 +46,7 @@ UNS
 	//137 : Corde Sortie
 	//Jusqu'a 140 : Objets permettant de s'enfuir poké  sauvage
 	//Enfin, tout ce qui est Item a tenir.
-	Item::itemsLst[150] = new Item("Croc Rasoir", false, false, true, BagCat::OBJETS, 3);//En fait 3 c'est ???
+	Item::itemsLst[150] = new Item("Croc Rasoir", false, false, true, BagCat::OBJECTS, 3);//En fait 3 c'est ???
 	//Jusqu'aux ct : Item a tenir
 	//Après: CT*/ //NINTENDO(c)
         }
