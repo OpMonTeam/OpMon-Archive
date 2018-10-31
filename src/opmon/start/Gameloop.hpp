4d37f8934de72a8633795aa4ab8dcf2e768073a4
Bobor <bzk.bobor@gmail.com>
2018-10-31 21:54:01 +0100

    Ctrl memory safe with smart pointer

--- src/opmon/start/Gameloop.hpp
+++ src/opmon/start/Gameloop.hpp
@@ -8,6 +8,7 @@ File under GNU GPL v3.0
 #pragma once
 
 #include "../controller/AGameScreen.hpp"
+#include "../model/storage/UiData.hpp"
 #include "../view/Window.hpp"
 #include "./GameStatus.hpp"
 #include <memory>
@@ -18,21 +19,19 @@ namespace OpMon {
     class GameLoop {
       public:
         GameLoop();
-        ~GameLoop();
+        ~GameLoop() = default;
 
         GameStatus operator()();
 
         /**
-	 * Checks the event to know if the game must be stopped.
-	 * Returns GameStatus::STOP if escape is pressed or if the game is closed.
-	 * Returns GameStatus::CONTINUE if the game must continue.
-	 */
+         * Checks the event to know if the game must be stopped.
+         * Returns GameStatus::STOP if escape is pressed or if the game is closed.
+         * Returns GameStatus::CONTINUE if the game must continue.
+         */
         GameStatus _checkQuit(const sf::Event &event);
 
-      private:
-        View::Window window;
-
-        std::stack<Controller::AGameScreen *> _gameScreens;
+        std::unique_ptr<Model::UiData> uidata;
+        std::stack<std::unique_ptr<Controller::AGameScreen>> _gameScreens;
     };
 
 } // namespace OpMon
