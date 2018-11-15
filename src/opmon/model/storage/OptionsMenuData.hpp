0b0507c715056663955bb5f20afea73c3d1b520f
Bobor <bzk.bobor@gmail.com>
2018-11-16 00:32:16 +0100

    Ajout fonctionnel de la modification des commandes

--- src/opmon/model/storage/OptionsMenuData.hpp
+++ src/opmon/model/storage/OptionsMenuData.hpp
@@ -18,7 +18,9 @@ namespace OpMon {
             sf::Texture langBg;
             sf::Texture yesTx;
             sf::Texture creditsBg;
+            sf::Texture controlesBg;
             sf::Texture volumeCur;
+            sf::Texture keyChange;
 
             OptionsMenuData(OptionsMenuData const &);
 
@@ -28,7 +30,9 @@ namespace OpMon {
             sf::Texture const &getLangBg() const { return langBg; }
             sf::Texture const &getYesTx() const { return yesTx; }
             sf::Texture const &getCreditsBg() const { return creditsBg; }
+            sf::Texture const &getControlesBg() const { return controlesBg; }
             sf::Texture const &getVolumeCur() const { return volumeCur; }
+            sf::Texture const &getKeyChange() const { return keyChange; }
             UiData *getUiDataPtr() { return uidata; }
             OptionsMenuData(UiData *data);
         };
