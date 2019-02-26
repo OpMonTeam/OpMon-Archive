6455f0dd7b2e7b551f1094aad4eb9b5f74f84d74
Cyriel <jlppcprog@gmail.com>
2019-02-26 15:45:09 +0100

    Added Options Menu access from the in-game menu

--- src/opmon/controller/GameMenuCtrl.cpp
+++ src/opmon/controller/GameMenuCtrl.cpp
@@ -5,6 +5,9 @@ File under GNU GPL v3.0 license
 */
 
 #include "GameMenuCtrl.hpp"
+#include "OptionsMenuCtrl.hpp"
+
+#define LOAD_OPTIONS 1
 
 namespace OpMon {
     namespace Controller {
@@ -23,24 +26,34 @@ namespace OpMon {
 	GameStatus GameMenuCtrl::checkEvent(sf::Event const& event) {
 	    switch(event.type) {
 	    case sf::Event::KeyPressed:
-		if(event.key.code == sf::Keyboard::M){
-		    return GameStatus::PREVIOUS_NLS;
-		}
-		if(event.key.code == sf::Keyboard::Up){
-		    curPos -= 2;
-		}
-		if(event.key.code == sf::Keyboard::Down){
-		    curPos += 2;
-		}
-		if(event.key.code == sf::Keyboard::Left){
-		    curPos--;
-		}
-		if(event.key.code == sf::Keyboard::Right){
-		    curPos++;
-		}
-		view.setCurPos(curPos.getValue());
+			if(event.key.code == sf::Keyboard::M){
+				return GameStatus::PREVIOUS_NLS;
+			}
+			if(event.key.code == sf::Keyboard::Up){
+				curPos -= 2;
+			}
+			if(event.key.code == sf::Keyboard::Down){
+				curPos += 2;
+			}
+			if(event.key.code == sf::Keyboard::Left){
+				curPos--;
+			}
+			if(event.key.code == sf::Keyboard::Right){
+				curPos++;
+			}
+			if(event.key.code == sf::Keyboard::Return){
+				switch(curPos.getValue()){
+					case 5:
+						loadNext = LOAD_OPTIONS;
+						return GameStatus::NEXT_NLS;
+					default:
+						data.getUiDataPtr()->getJukebox().playSound("nope");
+						break;
+				}
+			}
+			view.setCurPos(curPos.getValue());
 	    default:
-		break;
+			break;
 	    }
 
 	    return GameStatus::CONTINUE;
@@ -51,7 +64,13 @@ namespace OpMon {
 	}
 
 	void GameMenuCtrl::loadNextScreen() {
-	   
+	   switch(loadNext){
+		   case LOAD_OPTIONS:
+                _next_gs = std::make_unique<OptionsMenuCtrl>(data.getUiDataPtr());
+                break;
+           default:
+                handleError("Error : Unknown view to load in MainMenuCtrl.", true);
+	   }
 	}
 
 	void GameMenuCtrl::suspend() {
