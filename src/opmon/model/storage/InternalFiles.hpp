db33d1306a6d50b9c4c8f910fdd18946ef76b718
CyrielCentori <jlppcprog@ntymail.com>
2019-03-13 21:54:11 +0100

    Fixed collision bug on linux

--- src/opmon/model/storage/InternalFiles.hpp
+++ src/opmon/model/storage/InternalFiles.hpp
@@ -26,37 +26,6 @@ namespace OpMon {
         namespace Collisions {
             extern int colTile[4096];
         }
-
-        //TODO : Put the maps in XML files
-        namespace Maps {
-            extern int feLayer1[2304];
-            extern int feLayer2[2304];
-            extern int feLayer3[2304];
-            extern int laboLayer1[512];
-            extern int laboLayer2[512];
-            extern int laboLayer3[512];
-            extern int pproomLayer1[72];
-            extern int pproomLayer2[72];
-            extern int pproomLayer3[72];
-            extern int pphomeLayer1[256];
-            extern int pphomeLayer2[256];
-            extern int pphomeLayer3[256];
-            extern int momroomLayer1[42];
-            extern int momroomLayer2[42];
-            extern int momroomLayer3[42];
-            extern int rivalhomeLayer1[256];
-            extern int rivalhomeLayer2[256];
-            extern int rivalhomeLayer3[256];
-            extern int route14Layer1[4902];
-            extern int route14Layer2[4902];
-            extern int route14Layer3[4902];
-            extern int myciLayer1[1225];
-            extern int myciLayer2[1225];
-            extern int myciLayer3[1225];
-            extern int opcenterLayer1[324];
-            extern int opcenterLayer2[324];
-            extern int opcenterLayer3[324];
-        } // namespace Maps
     }     // namespace Model
 } // namespace OpMon
 #endif // INTERNALFILES_HPP
