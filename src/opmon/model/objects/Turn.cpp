0cb4e715d84120790f1b9ba3282f7e587412cfb1
Cyriel <jlppcprog@ntymail.com>
2019-03-28 19:42:45 +0000

    Reindented code

--- src/opmon/model/objects/Turn.cpp
+++ src/opmon/model/objects/Turn.cpp
@@ -12,24 +12,24 @@ namespace OpMon {
         void newTurnAction(TurnAction *toNew) {
             toNew->hpLost = 0;
             toNew->dialog = std::vector<Utils::OpString>();
-			toNew->type = TurnActionType::NOTHING;
-			toNew->statCoef = 0;
+            toNew->type = TurnActionType::NOTHING;
+            toNew->statCoef = 0;
         }
-		
-		void newTurnData(TurnData *toNew) {
-			toNew->opmon = nullptr;
-			toNew->attackUsed = nullptr;
-			toNew->type = TurnType::ATTACK;
-			toNew->itemUsed = nullptr;
-		}
-		
-		TurnAction createTurnDialogAction(std::vector<Utils::OpString> dialog){
-			TurnAction ta;
-			newTurnAction(&ta);
-			ta.type = TurnActionType::DIALOG;
-			ta.dialog = dialog;
-			return ta;
-		}
+
+        void newTurnData(TurnData *toNew) {
+            toNew->opmon = nullptr;
+            toNew->attackUsed = nullptr;
+            toNew->type = TurnType::ATTACK;
+            toNew->itemUsed = nullptr;
+        }
+
+        TurnAction createTurnDialogAction(std::vector<Utils::OpString> dialog) {
+            TurnAction ta;
+            newTurnAction(&ta);
+            ta.type = TurnActionType::DIALOG;
+            ta.dialog = dialog;
+            return ta;
+        }
 
     } // namespace Model
 } // namespace OpMon
