483d4b0a5c1d2dfbe30be9f3e03a0bdd85d3b2ef
Cyrion <jlppcprog@ntymail.com>
2019-04-21 10:29:19 +0200

    Added Movement class

--- src/opmon/view/Elements.hpp
+++ src/opmon/view/Elements.hpp
@@ -6,7 +6,6 @@ File under GNU GPL v3.0 license
 */
 #ifndef ELEMENTS_HPP
 #define ELEMENTS_HPP
-#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <vector>
@@ -25,6 +24,42 @@ namespace OpMon {
           public:
             MapLayer(sf::Vector2i size, const int tilesCode[]);
         };
+
+	class Movement {
+	private:
+	    /* When the movement ends, from 0 to this. Set to 0 for no limit */
+	    const unsigned int time;
+	    /* Formula exemple : {2, 8, 1, 6, 9} will be used as 2 + 8t + t² + 6t^3 + 9t^4 */
+	    const std::vector<int> xformula;
+	    const std::vector<int> yformula;
+	    /* If true, the movement will be relative to the object's position */
+	    const bool relative;
+	    /* Sprite to move */
+	    sf::Sprite* sprite;
+	    unsigned int t = 0;
+	public :
+	    
+	    Movement(std::vector<int> const& xformula, std::vector<int> const& yformula, int const& time, bool const& relative = true, sf::Sprite* sprite = nullptr);
+
+	    /* Attack a sprite to the object. Set replace to true to replace the current sprite if there is one.
+	       Returns a pointer to the old sprite, nullptr if it was empty or if replace = false and one was already present */
+	    sf::Sprite* attach(sf::Sprite* sprite, bool replace = false);
+
+	    /* Detach the current sprite. Returns a pointer to the old sprite, nullptr if there were none */
+	    sf::Sprite* detach();
+
+	    /* Returns true if no sprite is attached, false otherwise */
+	    bool empty();
+
+	    /* Apply the movement to the attached sprite, incrementing t and calculating the new coordinates. Returns false if :
+	       - t reached time
+	       - no sprite is attached
+	       Returns true otherwise
+	    */
+	    bool apply();
+		
+	};
+
 
     } // namespace View
 } // namespace OpMon
