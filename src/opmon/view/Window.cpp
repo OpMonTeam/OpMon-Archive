9458c850ba21eb2c4165b53c09ff435363fcbe49
PyroFlareX <31327816+PyroFlareX@users.noreply.github.com>
2019-05-23 20:45:47 -0700

    Started To Implement Resize Support

--- src/opmon/view/Window.cpp
+++ src/opmon/view/Window.cpp
@@ -1,7 +1,7 @@
 /*
 Window.cpp
 Author : Cyriel
-Contributors : BAKFR, torq
+Contributors : BAKFR, torq, PyroFlareX
 File under GNU GPL v3.0 license
 */
 #include "Window.hpp"
@@ -10,6 +10,7 @@ File under GNU GPL v3.0 license
 #include "../model/save/OptionsSave.hpp"
 #include "../model/storage/ResourceLoader.hpp"
 #include "../start/Core.hpp"
+#include "../start/GameStatus.hpp"
 
 #include <SFML/Graphics.hpp>
 
@@ -22,7 +23,7 @@ namespace OpMon {
             if(!Model::OptionsSave::checkParam("fullscreen")) {
                 Model::OptionsSave::addOrModifParam("fullscreen", "false");
             }
-	    //settings.antialiasingLevel = 1;
+	        //settings.antialiasingLevel = 1;
             if(Model::OptionsSave::getParam("fullscreen").getValue() == "true") {
                 fullScreen = true;
                 window.create(sf::VideoMode::getFullscreenModes().at(0), "OpMon Lazuli", sf::Style::Fullscreen, settings);
@@ -34,11 +35,11 @@ namespace OpMon {
             Model::ResourceLoader::load(icon, "opmon.png");
             window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
 
-            frame.create(512, 512);
+            frame->create(window.getSize().x, window.getSize().y);
 
             oplog("Window initialized!");
             //window.setVerticalSyncEnabled(true);
-            window.setFramerateLimit(60);
+            window.setFramerateLimit(30);
             window.setKeyRepeatEnabled(false);
         }
 
@@ -48,16 +49,17 @@ namespace OpMon {
             oplog("Window closed. No error detected. Goodbye.");
         }
 
-	void Window::reboot(){
-	    close();
-	    open();
-	}
+	    void Window::reboot(){
+	        close();
+	        open();
+    	}
 	
 
         void Window::refresh() {
-            frame.display();
-	    sf::Texture frameTexture = frame.getTexture();
-	    frameTexture.setSmooth(true);
+            frame->display();
+	        sf::Texture frameTexture = frame->getTexture();
+	        frameTexture.setSmooth(true);
+            frameTexture.setRepeated(true);
             sf::Sprite sprite(frameTexture);
 	    
             if(fullScreen) {
@@ -65,26 +67,28 @@ namespace OpMon {
                 sprite.setScale(coef, coef);
                 sprite.setPosition(((window.getSize().x / 2) - (sprite.getGlobalBounds().width / 2)), 0);
             }
-					sf::Event event;
-					while (window.pollEvent(event))
-					{
-   				 if (event.type == sf::Event::Close)
-					 {
-						 window.close();
-					 }
+            window.clear(sf::Color::Black);
+            window.draw(sprite);
+            window.display();
+        }
+        void Window::handleEvent(sf::Event* event){
 
-    			// catch the resize events
-          if (event.type == sf::Event::Resized)
-          {
-     	    // update the view to the new size of the window
-     	    sf::FloatRect visibleArea(0.f, 0.f, event.size.width, event.size.height);
-    	    window.setView(sf::View(visibleArea));
-  		    }
-          }
+            // catch the resize events
+            if (event->type == sf::Event::Resized)
+            {
+                // update the view to the new size of the window
+                sf::FloatRect visibleArea(0.f, 0.f, event->size.width, event->size.height);
 
-           window.clear(sf::Color::Black);
-           window.draw(sprite);
-           window.display();
+                sf::View view(visibleArea);
+
+                delete frame;
+                frame = new sf::RenderTexture();
+                frame->create(window.getSize().x, window.getSize().y);
+                //frame->setView(view);
+
+                window.setView(view);
+            }
+        
         }
 
     } // namespace View
