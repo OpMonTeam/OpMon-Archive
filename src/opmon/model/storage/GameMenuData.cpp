01ef5bf12f6eadd5454a98d831efb564e79c4b24
Navet56 <evandib@gmail.com>
2019-08-08 21:55:04 +0200

    Updated In-Game menu

--- src/opmon/model/storage/GameMenuData.cpp
+++ src/opmon/model/storage/GameMenuData.cpp
@@ -15,11 +15,11 @@ namespace OpMon {
             ResourceLoader::load(menuTexture, "backgrounds/menu.png");
             ResourceLoader::loadTextureArray(selectionTexture, "backgrounds/menuS%d.png", 6, 1);
 
-            selectionPos[0] = sf::Vector2f(120, 76);
+            selectionPos[0] = sf::Vector2f(106, 77);
             selectionPos[1] = sf::Vector2f(252, selectionPos[0].y);
-            selectionPos[2] = sf::Vector2f(selectionPos[0].x - 24, selectionPos[0].y + 96);
-            selectionPos[3] = sf::Vector2f(selectionPos[1].x, selectionPos[2].y);
-            selectionPos[4] = sf::Vector2f(selectionPos[0].x, selectionPos[2].y + 136);
+            selectionPos[2] = sf::Vector2f(selectionPos[0].x - 9, selectionPos[0].y + 100);
+            selectionPos[3] = sf::Vector2f(selectionPos[1].x, selectionPos[2].y - 4);
+            selectionPos[4] = sf::Vector2f(selectionPos[0].x, selectionPos[2].y + 115);
             selectionPos[5] = sf::Vector2f(selectionPos[1].x, selectionPos[4].y);
         }
 
