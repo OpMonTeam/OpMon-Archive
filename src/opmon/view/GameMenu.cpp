9bef82333f447d8f5258b4b29bcb7b27417d324f
CyrielleCentori <jlppcprog@ntymail.com>
2019-08-15 12:41:18 +0200

    Added text to the game menu

--- src/opmon/view/GameMenu.cpp
+++ src/opmon/view/GameMenu.cpp
@@ -5,6 +5,7 @@ File under GNU GPL v3.0 license
 */
 
 #include "GameMenu.hpp"
+#include "../../utils/StringKeys.hpp"
 
 namespace OpMon {
     namespace View {
@@ -16,7 +17,25 @@ namespace OpMon {
             for(size_t i = 0; i < 6; i++) {
                 selections[i].setTexture(data.getSelectionTexture(i));
                 selections[i].setPosition(data.getSelectionPos(i));
+                selectxt[i].setFont(data.getUiDataPtr()->getFont());
+                selectxt[i].setCharacterSize(22);
+                selectxt[i].setSfmlColor(sf::Color::White);
             }
+            selectxt[0].setString(Utils::StringKeys::get("gamemenu.save"));
+            selectxt[1].setString(Utils::StringKeys::get("gamemenu.id"));
+            selectxt[2].setString(Utils::StringKeys::get("gamemenu.bag"));
+            selectxt[3].setString(Utils::StringKeys::get("gamemenu.opmons"));
+            selectxt[4].setString(Utils::StringKeys::get("gamemenu.opdex"));
+            selectxt[5].setString(Utils::StringKeys::get("gamemenu.settings"));
+
+            selectxt[0].setPosition(150, 140);
+            selectxt[1].setPosition(260, 140);
+            selectxt[2].setPosition(115, 225);
+            selectxt[3].setPosition(312, 225);
+            selectxt[4].setPosition(150, 310);
+            selectxt[5].setPosition(260, 310);
+
+
         }
 
         void GameMenu::setCurPos(int newCurPos) {
@@ -29,6 +48,9 @@ namespace OpMon {
             frame.draw(background);
             frame.draw(menuBg);
             frame.draw(selections[curPos]);
+            for(size_t i = 0; i < 6; i++){
+              frame.draw(selectxt[i]);
+            }
             return GameStatus::CONTINUE;
         }
 
