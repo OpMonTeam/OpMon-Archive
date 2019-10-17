264dbb28efe696cda6d860d7b180fc46d994eb2f
Cyrielle <jlppcprog@ntymail.com>
2019-10-17 16:49:47 +0200

    Started documentation (time.hpp, StringKeys.hpp)

--- src/utils/time.hpp
+++ src/utils/time.hpp
@@ -1,35 +1,41 @@
-/*
-time.hpp
-Author : BAKFR
-File under GNU GPL v3.0
-*/
+/*!
+ * \file time.hpp
+ * \brief Time-related utilities.
+ * \author BAKFR
+ * \date 08/01/18
+ * \copyright GNU GPL v3.0
+ */
 #pragma once
 
+/*! \namespace Utils
+ *  \brief Contains different utilities.
+ */
 namespace Utils {
-    /**
-   * Utilities for everything time-related.
-   */
+    /*!
+	 * \namespace Time
+     * \brief Contains time-related utilities.
+	 */
     namespace Time {
 
-        /**
-	 * Set (or reset) the reference clock to zero.
-	 */
+        /*! \fn void initClock()
+		 *  \brief Set (or reset) the reference clock to zero.
+		 */
         void initClock();
 
-        /**
-	 * Return the number of elapsed milliseconds since the beginning of the program
-	 */
+        /*! \fn int getElapsedMilliseconds()
+		 *  \return The number of elapsed milliseconds since the beginning of the program.
+		 */
         int getElapsedMilliseconds();
 
-        /**
-         * Return the number of elapsed seconds since the beginning of the program
+        /*! \fn float getElapsedSeconds()
+         *  \return The number of elapsed seconds since the beginning of the program.
          */
         float getElapsedSeconds();
 
-        /**
-	 * Sleep for determined duration.
-	 * @param duration - time to wait, in milliseconds
-	 */
+        /*! \fn void wait(int duration)
+		*   \brief Pauses the program for a determined duration.
+		*   \param duration Time to wait, in milliseconds
+		*/
         void wait(int duration);
 
     } // namespace Time
