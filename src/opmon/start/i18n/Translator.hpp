5475b2fb99e14311d3799ae992386c0835ad82ab
CyrielleCentori <jlppcprog@ntymail.com>
2019-10-20 22:46:30 +0200

    Documented i18n source code

--- src/opmon/start/i18n/Translator.hpp
+++ src/opmon/start/i18n/Translator.hpp
@@ -1,8 +1,8 @@
-/**
-Translator.hpp
-Author : BAKfr
-File under GNU GPL v3.0 license
-*/
+/*!
+ * \file Translator.hpp
+ * \author BAKfr
+ * \copyright GNU GPL v3.0
+ */
 #pragma once
 
 #include <map>
@@ -14,13 +14,14 @@ namespace OpMon {
 
         class ATranslatable;
 
-        /**
-     * Class in charge of the lang setting. It load (and reload) language file.
-     *
-     * When the lang changes, it informs all `Translatable` instances registered with `subscribe()`.
-     *
-     * Note: This class is a singleton. It can only be acceded by using `getInstance()`.
-     */
+        /*!
+         * \class Translator Translator.hpp "start/opmon/i18n/Translator.hpp"
+         * \brief Class in charge of the lang setting. It load (and reload) language file.
+         *
+         * When the lang changes, it informs all Translatable instances registered with subscribe().
+         *
+         * \note This class is a singleton. It can only be acceded by using getInstance().
+         */
         class Translator {
           public:
             //Ensure Singleton can't be copied.
@@ -29,15 +30,22 @@ namespace OpMon {
 
             static Translator &getInstance();
 
-            /**
-       * Set the language. It MUST be called at start to set the first language.
-       *
-       * @param lang_code should be "en", "es" or "fr".
-       */
+            /*!
+             * \brief Sets the language.
+             * \warning It MUST be called at start to set the first language.
+             *
+             * \param lang_code should be "en", "es" or "fr".
+             */
             void setLang(const std::string &lang_code);
 
+            /*!
+             * \return The current language.
+             */
             const std::string &getLang();
 
+            /*!
+             * \return A map of available languages, the first element being the language code, and the second the name of the language in said language.
+             */
             const std::map<const std::string, const std::string> getAvailableLanguages();
 
             void subscribe(ATranslatable *listener);
