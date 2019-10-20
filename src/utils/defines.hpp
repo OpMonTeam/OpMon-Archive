14b3a98aeeae27cb2f43e40c66ce9a3813a78e9b
CyrielleCentori <jlppcprog@ntymail.com>
2019-10-20 21:52:45 +0200

    Documented src/utils

--- src/utils/defines.hpp
+++ src/utils/defines.hpp
@@ -1,8 +1,9 @@
-/*
-defines.hpp
-Author : Cyrielle
-Contributor : BAKFR
-File under GNU GPL v3.0 license
+/*!
+ * \file defines.hpp
+ * \authors Cyrielle
+ * \authors BAKFR
+ * \copyright GNU GPL v3.0 license
+ * \date 31/10/17
 */
 #ifndef DEFINES_HPP
 #define DEFINES_HPP
@@ -14,22 +15,38 @@ File under GNU GPL v3.0 license
 #elif defined(_MSC_VER)
 #define OP_DEPRECATED __declspec(deprecated)
 #else
+/*!
+ * \brief Indicates the deprecation of an object.
+ */
 #define OP_DEPRECATED
 #endif
 
 #ifdef SFML_COMPATIBILITY
 #define setSfmlColor setColor
 #else
+/*!
+ * \brief Compatibility macro to avoir deprecation warning with earlier SFMl versions.
+ * \details Redirects to setFillColor() if SFML_COMPATIBILITY is declared, or setColor() otherwise.
+ */
 #define setSfmlColor setFillColor
 #endif
 
 #include <string>
 
-/* location of the save folder */
+/*!
+ * \brief Returns the location of the save path folder.
+ * \deprecated Use Utils::Path::getSavePath().
+ */
 #define SAVE_PATH Utils::Path::getSavePath()
 
+/*!
+ * \brief Contains the default size font.
+ */
 #define FONT_SIZE_DEFAULT 25
 
+/*!
+ * \brief Contains the number PI with 11 digits.
+ */
 #define PI 3.1415926535
 
 #endif
