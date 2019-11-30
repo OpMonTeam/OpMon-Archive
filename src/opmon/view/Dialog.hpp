710a2d1cda5240a661081189f6c016352b0d1ac3
CyrielleCentori <jlppcprog@ntymail.com>
2019-11-30 13:11:43 +0100

    Documented Dialog

--- src/opmon/view/Dialog.hpp
+++ src/opmon/view/Dialog.hpp
@@ -1,8 +1,8 @@
-/*
-  Dialog.hpp
-  Author : BAKFR
-  Contributor : Cyrielle
-  File under GNU GPL 3.0 licence
+/*!
+ * \file Dialog.hpp
+ * \authors BAKFR
+ * \authors Cyrielle
+ * \copyright GNU GPL 3.0
 */
 #ifndef OPMON_DIALOG_CPP_HPP
 #define OPMON_DIALOG_CPP_HPP
@@ -19,26 +19,40 @@ namespace OpMon {
 
         class Dialog {
           private:
-            /** array of all lines composing the dialog. */
+            /*!
+             * \brief Array of all lines composing the dialog.
+             */
             std::queue<sf::String> text;
 
-            /** The 3 lines currently displayed. */
+            /*!
+             * \brief The 3 lines currently displayed.
+             */
             sf::String currentTxt[3] = {sf::String(" "), sf::String(" "), sf::String(" ")};
 
-            /**
-             * If true, the dialog is fully displayed by the text animation, and the
+            /*!
+             * \brief Checks if the dialog box is full.
+             * \details If `true`, the dialog is fully displayed by the text animation, and the
              * next step (on user action) would be to display the next lines.
             */
             bool changeDialog = false;
 
-            /** Number of completely displayed lines. */
+            /*!
+             * \brief Number of completely displayed lines.
+             */
             unsigned int line = 0;
-            /** index of the next character to display by the text animation. */
+            /*!
+             * \brief Index of the next character to display by the text animation.
+             */
             unsigned int i = 0;
 
-            /** Set to `true` when the entire dialog has been displayed. */
+            /*!
+             * \brief Set to `true` when the entire dialog has been displayed.
+             */
             bool is_dialog_over = false;
 
+            /*!
+             * \brief The sound played when a dialog is passed.
+             */
             sf::Sound dialogPass;
 
             Model::UiData *uidata;
@@ -47,45 +61,58 @@ namespace OpMon {
 
             void init();
 
-          public:
-            OP_DEPRECATED Dialog(std::vector<sf::String> text, Model::UiData *uidata);
-            Dialog(std::queue<sf::String> text, Model::UiData *uidata);
-            Dialog(sf::String text, Model::UiData *uidata);
-
-            /**
-       * Move forward in a dialog, in response to an event like a space key pressed.
-       *
-       * If the current dialog lines aren't fully displayed (text animation
-       * ongoing), terminate the animation, wait 50ms, then ask for a dialog change.
-       * If the animation is over, and there still remaining lines to display,
-       * play a sound (dialogPass) then pass to the next lines.
-       * If there is no more line to display, the dialog is over.
-       */
-            void pass();
-
-            /**
-       * Display the dialog character by character until the 3 lines are fully displayed.
-       */
-            void updateTextAnimation();
-
-            /**
-       * Draw the dialog on the main frame.
-       */
-            void draw(sf::RenderTarget &frame);
-
-            /**
-       * @return `true` is the entire dialog has been displayed; `false` otherwise.
-       */
-            bool isDialogOver();
-
-            /**
-				Set the background visible or invisible, used in screens using other dialog boxes.
-			*/
-            void setBackgroundVisible(bool visible);
-
             sf::Sprite arrDial;
             sf::Text dialogText[3];
             sf::Sprite background;
+
+          public:
+            /*!
+             * \brief Initialises a dialog with an array of texts to print.
+             * \param text An array of texts. One element = one line.
+             * \deprecated Use std::queue instead of std::vector, or send one sf::String directly.
+             */
+            OP_DEPRECATED Dialog(std::vector<sf::String> text, Model::UiData *uidata);
+            /*!
+             * \brief Initises a dialog with a queue of texts to print.
+             * \param text A queue of texts. One element = one line.
+             */
+            Dialog(std::queue<sf::String> text, Model::UiData *uidata);
+            /*!
+             * \brief Initialises a dialog.
+             * \param text The text of the dialog.
+             */
+            Dialog(sf::String text, Model::UiData *uidata);
+
+            /*!
+             * \brief Move forward in a dialog, in response to an event like a space key pressed.
+             *
+             * If the current dialog lines aren't fully displayed (text animation
+             * ongoing), terminate the animation, wait 50ms, then ask for a dialog change.
+             * If the animation is over, and there still remaining lines to display,
+             * play a sound (dialogPass) then pass to the next lines.
+             * If there is no more line to display, the dialog is over.
+             */
+            void pass();
+
+            /*!
+             * \brief Display the dialog character by character until the 3 lines are fully displayed.
+             */
+            void updateTextAnimation();
+
+            /*!
+             * \brief Draw the dialog on the main frame.
+             */
+            void draw(sf::RenderTarget &frame);
+
+            /*!
+             * \return `true` is the entire dialog has been displayed; `false` otherwise.
+             */
+            bool isDialogOver();
+
+            /*!
+             * \brief Set the background visible or invisible, used in screens using other dialog boxes.
+             */
+            void setBackgroundVisible(bool visible);
         };
     } // namespace View
 } // namespace OpMon
