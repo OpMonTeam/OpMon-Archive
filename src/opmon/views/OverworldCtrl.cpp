ab60c7e6de409fc25a317eb5c5a7b0c2cb6c8b4a
CyrielleCentori <cyriellecentori@uymail.com>
2020-02-09 17:25:03 +0100

    Reoganized directories + Moved PlayerCtrl and EventsCtrl to OverworldCtrl

--- src/opmon/views/OverworldCtrl.cpp
+++ src/opmon/views/OverworldCtrl.cpp
@@ -12,7 +12,6 @@ File under GNU GPL v3.0 license
 #include "AnimationCtrl.hpp"
 #include "BattleCtrl.hpp"
 #include "GameMenuCtrl.hpp"
-#include "PlayerCtrl.hpp"
 
 //Defines created to make the code easier to read
 #define LOAD_BATTLE 1
@@ -127,8 +126,8 @@ namespace OpMon {
         }
 
         GameStatus OverworldCtrl::checkEventsNoDialog(sf::Event const &event, Model::Player &player) {
-            EventsCtrl::checkAction(event, player, view);
-            Controller::PlayerCtrl::checkMove(player, view);
+            checkAction(event, player, view);
+            checkMove(player, view);
 
             if(view.getBattleDeclared() != nullptr) {
                 if(view.getBattleDeclared()->isDefeated()) {
@@ -145,7 +144,7 @@ namespace OpMon {
         GameStatus OverworldCtrl::update(sf::RenderTexture &frame) {
             bool is_dialog_open = view.getDialog() && !view.getDialog()->isDialogOver();
             if(!is_dialog_open) {
-                EventsCtrl::updateEvents(data.getMap(player.getMapId())->getEvents(), player, view);
+                updateEvents(data.getMap(player.getMapId())->getEvents(), player, view);
             }
 
             GameStatus toReturn = view(frame);
@@ -181,6 +180,97 @@ namespace OpMon {
 
         void OverworldCtrl::resume() {
             data.getUiDataPtr()->getJukebox().play(data.getCurrentMap()->getBg());
+        }
+
+        void OverworldCtrl::checkMove(Model::Player &player, View::Overworld &overworld) {
+            if(!overworld.justTp && !player.getPosition().isAnim() && !player.getPosition().isLocked()) {
+                //TODO Factorise code
+                if(sf::Keyboard::isKeyPressed(overworld.getData().getUiDataPtr()->getKeyUp())) {
+                    overworld.startPlayerAnimation();
+                    move(Model::Side::TO_UP, player, overworld);
+                } else if(sf::Keyboard::isKeyPressed(overworld.getData().getUiDataPtr()->getKeyDown())) {
+                    overworld.startPlayerAnimation();
+                    move(Model::Side::TO_DOWN, player, overworld);
+                } else if(sf::Keyboard::isKeyPressed(overworld.getData().getUiDataPtr()->getKeyLeft())) {
+                    overworld.startPlayerAnimation();
+                    move(Model::Side::TO_LEFT, player, overworld);
+                } else if(sf::Keyboard::isKeyPressed(overworld.getData().getUiDataPtr()->getKeyRight())) {
+                    overworld.startPlayerAnimation();
+                    move(Model::Side::TO_RIGHT, player, overworld);
+                }
+            }
+        }
+
+        void OverworldCtrl::move(Model::Side direction, Model::Player &player, View::Overworld &overworld) {
+            player.getPosition().move(direction, overworld.getData().getCurrentMap());
+
+            Model::Map *map = overworld.getData().getCurrentMap();
+            auto eventList = map->getEvent(player.getPosition().getPosition());
+            actionEvents(eventList, player, Model::Events::EventTrigger::GO_IN, overworld);
+        }
+
+        void OverworldCtrl::checkAction(sf::Event const &event, Model::Player &player, View::Overworld &overworld) {
+            //If the player isn't moving, then this checks if the player want to activate an event.
+            if(!player.getPosition().isAnim()) {
+                //Get the event coordinates and activate it if the player interacted with it.
+                if(sf::Keyboard::isKeyPressed(overworld.getData().getUiDataPtr()->getKeyInteract())) {
+                    int lx = player.getPosition().getPosition().x;
+                    int ly = player.getPosition().getPosition().y;
+                    switch(player.getPosition().getDir()) {
+                    case Model::Side::TO_UP:
+                        ly--;
+                        break;
+                    case Model::Side::TO_DOWN:
+                        ly++;
+                        break;
+                    case Model::Side::TO_LEFT:
+                        lx--;
+                        break;
+                    case Model::Side::TO_RIGHT:
+                        lx++;
+                        break;
+                    default:
+                        break;
+                    }
+
+                    std::vector<Model::Event *> eventList = overworld.getData().getCurrentMap()->getEvent(sf::Vector2i(lx, ly));
+                    actionEvents(eventList, player, Model::Events::EventTrigger::PRESS, overworld);
+                }
+            }
+
+            //Searches for events at the same position as the player and activates them if they are triggered when the playeris in them.
+            if(!player.getPosition().isMoving()) {
+                std::vector<Model::Event *> eventList = overworld.getData().getCurrentMap()->getEvent(player.getPosition().getPosition());
+                actionEvents(eventList, player, Model::Events::EventTrigger::BE_IN, overworld);
+            }
+        }
+
+        void OverworldCtrl::actionEvents(std::vector<Model::Event *> &events, Model::Player &player, Model::Events::EventTrigger toTrigger, View::Overworld &overworld) {
+            //Checks if the player points at the right direction to activate the events. If yes, calls the events' action methods.
+            Model::Side ppDir = player.getPosition().getDir();
+            for(unsigned int i = 0; i < events.size(); i++) {
+                if(events[i]->getEventTrigger() == toTrigger) {
+                    bool go = false;
+                    if(((events[i]->getSide() & SIDE_UP) == SIDE_UP) && ppDir == Model::Side::TO_UP) {
+                        go = true;
+                    } else if(((events[i]->getSide() & SIDE_DOWN) == SIDE_DOWN) && ppDir == Model::Side::TO_DOWN) {
+                        go = true;
+                    } else if(((events[i]->getSide() & SIDE_RIGHT) == SIDE_RIGHT) && ppDir == Model::Side::TO_RIGHT) {
+                        go = true;
+                    } else if(((events[i]->getSide() & SIDE_LEFT) == SIDE_LEFT) && ppDir == Model::Side::TO_LEFT) {
+                        go = true;
+                    }
+                    if(go) {
+                        events[i]->action(player, overworld);
+                    }
+                }
+            }
+        }
+
+        void OverworldCtrl::updateEvents(std::vector<Model::Event *> &events, Model::Player &player, View::Overworld &overworld) {
+            for(Model::Event *event : events) {
+                event->update(player, overworld);
+            }
         }
 
     } // namespace Controller
