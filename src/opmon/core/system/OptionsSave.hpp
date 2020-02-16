eb02774383ca3a9e880951b9bbf74f66f83bba4f
CyrielleCentori <cyriellecentori@uymail.com>
2020-02-16 16:28:42 +0100

    Made OptionsSave a class (static methods)

--- src/opmon/core/system/OptionsSave.hpp
+++ src/opmon/core/system/OptionsSave.hpp
@@ -26,41 +26,42 @@
 
 namespace OpMon {
     namespace System {
+        /**
+         * \brief Defines a parameter.
+         */
+        class Param {
+        private:
+            std::string paramName;
+            std::string value;
+
+        public:
+            /*!
+             * \param name The parameter's name. Alphanumeric characters only.
+             * \param value The parameter's value.
+             */
+            Param(std::string name, std::string value);
+            std::string getName() const;
+            std::string getValue() const;
+            void setValue(std::string value);
+        };
+
         /*!
          *	\brief Contains the functions used to save the parameters.
-         *  \todo Make this a class.
+         *  \todo Make this an instantiable class.
          */
-        namespace OptionsSave {
-            /**
-             * \brief Defines a parameter.
-             */
-            class Param {
-              private:
-                std::string paramName;
-                std::string value;
-
-              public:
-                /*!
-                 * \param name The parameter's name. Alphanumeric characters only.
-                 * \param value The parameter's value.
-                 */
-                Param(std::string name, std::string value);
-                std::string getName() const;
-                std::string getValue() const;
-                void setValue(std::string value);
-            };
-
+        class OptionsSave {
+        public:
             /*!
              * \brief The list of the different parameters.
              * \todo Change this to an std::list ?
              */
-            extern std::vector<Param> paramList;
+            static std::vector<Param> paramList;
 
             /*!
              * \returns The parameter with this name in OptionsSave::paramList. Returns an empty parameter if none exists with the given name.
              * \param name The name of the parameter.
              */
-            Param getParam(std::string const &name);
+            static Param getParam(std::string const &name);
 
             /*!
              * \brief Sets the parameter with the given name to the given value.
@@ -68,14 +69,14 @@ namespace OpMon {
              * \param name The name of the parameter.
              * \param value The value to set the parameter to.
              */
-            void addOrModifParam(std::string const &name, std::string const &value);
+            static void addOrModifParam(std::string const &name, std::string const &value);
 
             /*!
              * \brief Deletes a parameter.
              * \param name The name of the parameter.
              * \returns The deleted parameter. If none exists with this name, returns an empty parameter.
              */
-            Param deleteParam(std::string const &name);
+            static Param deleteParam(std::string const &name);
 
             //.CPP ONLY :
             //int searchParam(string name);
@@ -84,22 +85,25 @@ namespace OpMon {
              * \brief Saves the parameters.
              * \param file The file in which save the paramters.
              */
-            void saveParams(std::string const &file);
+            static void saveParams(std::string const &file);
 
             /*!
              * \brief Loads the paramters.
              * \param file The file in which the parameters are stored.
              */
-            void initParams(std::string const &file);
+            static void initParams(std::string const &file);
 
             /*!
              * \brief Checks if a parameters exists.
              * \param name The name of the parameter.
              * \returns `true` if the parameters exists, `false` otherwise.
              */
-            bool checkParam(std::string const &name);
+            static bool checkParam(std::string const &name);
 
-        } // namespace OptionsSave
+        private:
+            static int searchParam(std::string const &name);
+
+        };
     }     // namespace System
 } // namespace OpMon
 #endif // OPTIONSSAVE_HPP
