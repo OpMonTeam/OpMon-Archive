4dd3d7fed171f0d20399e0f21ae285cce03ef065
Navet56 <evandib@gmail.com>
2020-02-28 22:34:43 +0100

    Fixes build-and-run.sh

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-set -e # exit on error
+
+#Apply the submodules (OpMon-Data)
+git submodule update --init --recursive
 
 #Change every Release in this file to Debug if you want to build the game in debug.
-md bin/Release && cd bin/Release
+mkdir bin
+mkdir bin/Release
+cd bin/Release
 rm -r GameData
 cp -r ../../OpMon-Data/GameData .
 cd ../..
