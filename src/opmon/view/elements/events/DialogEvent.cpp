11817c891cbd11e77c72464f285e7a97155ce952
CyrielleCentori <cyriellecentori@uymail.com>
2020-03-31 10:59:04 +0200

    Moved TalkingEvent + Renamed to DialogEvent

---
+++ src/opmon/view/elements/events/DialogEvent.cpp
@@ -0,0 +1,26 @@
+#include "DialogEvent.hpp"
+
+namespace OpMon {
+	namespace Elements {
+
+		DialogEvent::DialogEvent(std::vector<sf::Texture> &otherTextures, sf::Vector2f const &position, Utils::OpString const &dialogKey, int sides, EventTrigger eventTrigger, bool passable)
+		: AbstractEvent(otherTextures, eventTrigger, position, sides, passable)
+		, Utils::I18n::ATranslatable()
+		, dialogKey(dialogKey) {
+			this->onLangChanged();
+		}
+
+		void DialogEvent::onLangChanged() {
+			dialog.clear();
+			dialog = dialogKey.getString(stringkeys);
+		}
+
+		void DialogEvent::action(Player &player, Overworld &overworld) {
+			overworld.startDialog(this->dialog);
+		}
+
+		void DialogEvent::update(Player &player, Overworld &overworld) {
+		}
+
+	} /* namespace Elements */
+} /* namespace OpMon */
