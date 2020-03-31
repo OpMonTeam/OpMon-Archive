7f63be3dfb711223a7f33c003f8c7cd767f283ad
CyrielleCentori <cyriellecentori@uymail.com>
2020-03-31 12:27:42 +0200

    Added AnimationEvent

---
+++ src/opmon/view/elements/events/AnimationEvent.cpp
@@ -0,0 +1,34 @@
+#include "AnimationEvent.hpp"
+#include "src/utils/exceptions.hpp"
+
+namespace OpMon {
+	namespace Elements {
+
+		AnimationEvent::AnimationEvent(std::vector<sf::Texture> &otherTextures, EventTrigger eventTrigger, sf::Vector2f const &position, unsigned int framerate, bool loop, bool passable, int sides = SIDE_ALL)
+		: AbstractEvent(otherTextures, eventTrigger, position, sides, passable)
+		, framerate(framerate)
+		, loop(loop) {
+			if(framerate <= 0){
+				throw Utils::UnexpectedValueException(framerate, "a strictly positive integer in AnimationEvent", false);
+			}
+		}
+
+		void AnimationEvent::action(Player &player, Overworld &overworld){
+			playing = !(playing && loop); //Stops playing only if it's a loop. Else, stay playing or start playing.
+		}
+
+		void AnimationEvent::update(Player &player, Overworld &overworld){
+			if(playing && (framecount >= framerate)){
+				++currentTexture;
+				if(currentTexture == otherTextures.end()){
+					currentTexture = otherTextures.begin();
+					playing = loop; //If loop, continue playing, else, stop.
+				}
+			}else if(playing && (framecount < framerate)){
+				framecount++;
+			}
+		}
+
+
+	} /* namespace Elements */
+} /* namespace OpMon */
---
+++ src/opmon/view/elements/events/AnimationEvent.hpp
@@ -0,0 +1,44 @@
+/*!
+ * \file AnimationEvent.hpp
+ * \author Cyrielle
+ * \copyright GNU GPL v3.0
+ */
+
+#pragma once
+
+#include "AbstractEvent.hpp"
+
+namespace OpMon::Elements {
+
+	/*!
+	 * \brief An event that animates itself when triggered.
+	 */
+	class AnimationEvent : public AbstractEvent {
+	private:
+		/*!
+		 *  \brief The number of ticks elapsed between two frames.
+		 *
+		 *  \warn Can't be 0 or below.
+		 */
+		unsigned int framerate;
+
+		/*!
+		 * \brief If `true`, the animation will loop. A new action will stop the animation.
+		 */
+		bool loop;
+
+		/*!
+		 * \brief If the animation is currently playing.
+		 */
+		bool playing = false;
+
+		/*!
+		 * \brief Counter to know how many ticks has been elapsed since the last update.
+		 */
+		int framecount = 0;
+	public:
+		AnimationEvent(std::vector<sf::Texture> &otherTextures, EventTrigger eventTrigger, sf::Vector2f const &position, unsigned int framerate, bool loop, bool passable, int sides = SIDE_ALL);
+		void action(Player &player, Overworld &overworld);
+		void update(Player &player, Overworld &overworld);
+	};
+}
