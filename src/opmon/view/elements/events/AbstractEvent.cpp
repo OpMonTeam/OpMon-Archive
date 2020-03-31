a2755ab230d555b1a9a038b39e7893952d2c74df
CyrielleCentori <cyriellecentori@uymail.com>
2020-03-31 10:37:14 +0200

    Added AbstractEvent and MetaEvent

---
+++ src/opmon/view/elements/events/AbstractEvent.cpp
@@ -0,0 +1,21 @@
+#include "AbstractEvent.hpp"
+
+namespace OpMon {
+	namespace Elements {
+		AbstractEvent::AbstractEvent(std::vector<sf::Texture> &otherTextures, EventTrigger eventTrigger, sf::Vector2f const &position, int sides, bool passable)
+		: otherTextures(otherTextures)
+		, eventTrigger(eventTrigger)
+		, position(sf::Vector2f((position.x) * 32, (position.y) * 32))
+		, mapPos(position, true)
+		, passable(passable)
+		, sides(sides)
+		, sprite(std::make_unique<sf::Sprite>())
+		, currentTexture(otherTextures.begin()) {}
+
+		void AbstractEvent::updateTexture() {
+			this->sprite->setPosition(position);
+			this->sprite->setTexture(*currentTexture);
+		}
+	}
+}
+
