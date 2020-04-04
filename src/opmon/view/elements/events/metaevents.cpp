5075d15c0be361414849075553a9a42341dee03b
CyrielleCentori <cyriellecentori@uymail.com>
2020-04-04 16:35:08 +0200

    Reprogrammed TalkingCharaEvent

--- src/opmon/view/elements/events/metaevents.cpp
+++ src/opmon/view/elements/events/metaevents.cpp
@@ -2,19 +2,20 @@
 #include "AnimationEvent.hpp"
 #include "SoundEvent.hpp"
 #include "TPEvent.hpp"
+#include "DialogEvent.hpp"
 #include <queue>
 
 namespace OpMon::Elements {
 	DoorEvent::DoorEvent(OverworldData &data, std::string doorType, sf::Vector2f const &position, sf::Vector2i const &tpCoord, std::string const &map, EventTrigger eventTrigger, Side ppDir, int sides, bool passable)
 	: LinearMetaEvent(std::queue<AbstractEvent*>(std::deque<AbstractEvent*>({
 		new AnimationEvent(data.getDoorsTexture(doorType), eventTrigger, position, 1, false, passable, sides),
-		new SoundEvent(data.getUiDataPtr()->alpha, eventTrigger, position, doorType + " sound", false, false, sides, passable),
-		new TPEvent(data.getUiDataPtr()->alpha, eventTrigger, position, tpCoord, map, ppDir, sides, passable),
-		nullptr})),
-			std::queue<bool>(std::deque<bool>({false, true, false}))){
+				new SoundEvent(data.getUiDataPtr()->alpha, eventTrigger, position, doorType + " sound", false, false, sides, passable),
+				new TPEvent(data.getUiDataPtr()->alpha, eventTrigger, position, tpCoord, map, ppDir, sides, passable),
+				nullptr})),
+			std::queue<bool>(std::deque<bool>({false, true, false, false}))){
 		this->position += sf::Vector2f(0, -6); //TODO : Fix the sprites to get rid of this little fix
 		if(doorType == "shop door") {
-		   this->position.x -= 4;
+			this->position.x -= 4;
 		}
 	}
 
@@ -27,6 +28,49 @@ namespace OpMon::Elements {
 		LinearMetaEvent::update(player, overworld);
 		if(!processing){
 			player.getPosition().unlockMove();
+		}
+	}
+
+
+	TalkingCharaEvent::TalkingCharaEvent(std::vector<sf::Texture> &textures, sf::Vector2f const &position, Utils::OpString const &dialogKey, Side posDir, EventTrigger eventTrigger, MoveStyle moveStyle, std::vector<Side> predefinedPath, bool passable, int side)
+	: LinearMetaEvent(std::queue<AbstractEvent*>(std::deque<AbstractEvent*>({
+		new CharacterEvent(textures, position, posDir, moveStyle, eventTrigger, predefinedPath, passable, sides),
+				new DialogEvent(textures, position, dialogKey, sides, eventTrigger, passable),
+				nullptr})),
+			std::queue<bool>(std::deque<bool>({false, true, false}))) {
+
+	}
+
+	void TalkingCharaEvent::action(Player &player, Overworld& overworld){
+		mapPos.lockMove();
+		LinearMetaEvent::action(player, overworld);
+	}
+
+	void TalkingCharaEvent::update(Player &player, Overworld &overworld){
+		if(processing && !mapPos.isAnim()){
+			switch(player.getPosition().getDir()) { //Put the character's face in front of the player's one
+			case Side::TO_DOWN:
+				mapPos.setDir(Side::TO_UP);
+				break;
+			case Side::TO_UP:
+				mapPos.setDir(Side::TO_DOWN);
+				break;
+			case Side::TO_LEFT:
+				mapPos.setDir(Side::TO_RIGHT);
+				break;
+			case Side::TO_RIGHT:
+				mapPos.setDir(Side::TO_LEFT);
+				break;
+			default:
+				break;
+			}
+			//Put the correct texture to the NPC
+			currentTexture = otherTextures.begin() + (int)mapPos.getDir();
+			updateTexture();
+		}
+		LinearMetaEvent::update(player, overworld);
+		if(processing && !mapPos.isAnim()){
+			mapPos.unlockMove();
 		}
 	}
 }
