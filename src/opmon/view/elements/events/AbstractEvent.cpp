52ee827a4912fdf5f45b12308644cc2617eccdc5
CyrielleCentori <cyriellecentori@uymail.com>
2020-04-06 22:04:22 +0200

    Added Json constructors (For AbstractEvent, CharacterEvent, DialogEvent and TPEvent)

--- src/opmon/view/elements/events/AbstractEvent.cpp
+++ src/opmon/view/elements/events/AbstractEvent.cpp
@@ -13,6 +13,16 @@ namespace OpMon {
 		, sprite(std::make_unique<sf::Sprite>())
 		, currentTexture(otherTextures.begin()) {}
 
+		AbstractEvent::AbstractEvent(std::vector<sf::Texture> &textures, nlohmann::json jsonData)
+		: otherTextures(textures)
+		, eventTrigger(jsonData.value("trigger", EventTrigger::PRESS))
+		, position(sf::Vector2f(jsonData.value("position", std::vector<int>{0,0})[0], jsonData.value("position", std::vector<int>{0,0})[1]))
+		, mapPos(position, true)
+		, passable(jsonData.value("passable", false))
+		, sides(jsonData.value("side", SIDE_ALL))
+		, sprite(std::make_unique<sf::Sprite>())
+		, currentTexture(otherTextures.begin()) {}
+
 		void AbstractEvent::updateTexture() {
 			this->sprite->setPosition(position);
 			this->sprite->setTexture(*currentTexture);
